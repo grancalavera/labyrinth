9bf863050db531656dfabe70ec35379c0c31db53
Leon Coto <leoncoto@gmail.com>
2017-08-22 08:46:35 +0100

    Rough boad sketch

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,25 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+printBoard :: IO ()
+printBoard = putStrLn board
+
+
+board :: String
+board = concat $ ([
+  (concatMap cell [1..9]) ++ "\n",
+  (concatMap cell [1..9]) ++ "\n",
+  (concatMap cell [1..9]) ++ "\n",
+  (concatMap cell [1..9]) ++ "\n",
+  (concatMap cell [1..9]) ++ "\n",
+  (concatMap cell [1..9]) ++ "\n",
+  (concatMap cell [1..9]) ++ "\n",
+  (concatMap cell [1..9]) ++ "\n",
+  (concatMap cell [1..9]) ++ "\n"
+  ])
+
+cell :: Int -> String
+cell = \_ -> "[TOP] "
+
+-- T: type
+-- O: objective
+-- P: player
