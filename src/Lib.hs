c90b7ec012542893abacbb1e7016bfe3b09fbe86
Leon Coto <leoncoto@gmail.com>
2017-08-23 09:51:38 +0100

    Print grid with padding + cleanup

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,26 +5,46 @@ import Control.Monad (liftM)
 printBoard :: IO ()
 printBoard = putStrLn $ show board
 
+cols :: Integral a => a
+cols = 9
 type Cell = String
 data Board = Board [Cell] deriving Eq
 
 instance Show Board where
   show board =  intercalate "\n" (liftM (intercalate " ") $ toGrid board)
 
-board = Board (map toCell [1..(9*9)])
+board = Board (map toCell [1..(cols*cols)])
 
 toCell :: Int -> Cell
-toCell = \_ -> "[___]"
+toCell = \i -> "[" ++ (pad i) ++ "]"
 
 getBoard :: Board -> [Cell]
 getBoard (Board board) = board
+
+pad :: (Show a, Integral a) => a -> String
+pad i = pad' (show i)
+  where pad' s | length s > 2 = ""
+               | length s == 2 = s
+               | otherwise = pad' ("0"++s)
+
+padPoint = mapPoint pad
 
 toGrid :: Board -> [[Cell]]
 toGrid (Board board) = toGrid' board
   where
     toGrid' :: [Cell] -> [[Cell]]
-    toGrid' b = split (splitAt 9 b)
+    toGrid' b = split (splitAt cols b)
 
     split :: ([Cell], [Cell]) -> [[Cell]]
     split ([], _) = []
     split (xs, ys) = xs : toGrid' ys
+
+coords :: Integral a => a -> a -> (a, a)
+coords cols index = (x, y)
+  where x = index `mod` cols
+        y = index `div` cols
+
+getCoords = coords cols
+
+mapPoint :: Integral a => (a -> b) -> (a, a) -> (b, b)
+mapPoint f (x, y) = (f x, f y)
