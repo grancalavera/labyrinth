04ad69974a821676c4fbdb9db0fb87deb66e0294
Leon Coto <leoncoto@gmail.com>
2017-08-23 18:38:30 +0100

    Print corners and edges

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,8 +5,10 @@ import Control.Monad (liftM)
 printBoard :: IO ()
 printBoard = putStrLn $ show board
 
-cols :: Integral a => a
+cols :: Int
 cols = 9
+size :: Int
+size = cols - 1
 
 board :: Board
 board = Board (map toCell [0..(cols*cols)-1])
@@ -18,12 +20,15 @@ instance Show Board where
   show board =  intercalate "\n" (liftM (intercalate " ") $ toGrid board)
 
 toCell :: Int -> Cell
-toCell = \i -> "[" ++ (pad i) ++ "]"
+toCell i = (pad i) ++ "|" ++ cell
+  where cell | isCorner i = "***"
+             | isEdge i = " + "
+             | otherwise = "   "
 
 getBoard :: Board -> [Cell]
 getBoard (Board board) = board
 
-pad :: (Show a, Integral a) => a -> String
+pad :: (Show a, Num a) => a -> String
 pad i = pad' (show i)
   where pad' s | length s > 2 = ""
                | length s == 2 = s
@@ -41,12 +46,24 @@ toGrid (Board board) = toGrid' board
     split ([], _) = []
     split (xs, ys) = xs : toGrid' ys
 
-coords :: Integral a => a -> a -> (a, a)
+coords :: Int -> Int -> (Int, Int)
 coords cols index = (x, y)
   where x = index `rem` cols
         y = index `div` cols
 
 getCoords = coords cols
 
-mapPoint :: Integral a => (a -> b) -> (a, a) -> (b, b)
+mapPoint :: (Int -> b) -> (Int, Int) -> (b, b)
 mapPoint f (x, y) = (f x, f y)
+
+isEdge :: Int -> Bool
+isEdge i = isEdge' (getCoords i)
+  where isEdge' (x,y) = (x==0) || (y==0) || (x==size) || (y==size)
+
+isCorner :: Int -> Bool
+isCorner i = isCorner' (getCoords i)
+  where isCorner' (x,y) | (x==0)    && (y==0)    = True
+                        | (x==size) && (y==0)    = True
+                        | (x==size) && (y==size) = True
+                        | (x==0)    && (y==size) = True
+                        | otherwise              = False
