c27ad3df940b2c340202937b31b1078c2648f650
Leon Coto <leoncoto@gmail.com>
2017-10-04 13:18:15 +0100

    Reduce surface on app
    Move pretty much everything to src

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,2 @@
-module Main where
-import Lib
-import System.Console.ANSI
-
-main :: IO ()
-main = do
-  clearScreen
-  drawBackground
-
-  board <- initialBoard
-  drawBoard origin board
-
-  -- this should go elsewhere
-  setCursorPosition gameHeight 0
-
--- these coordinates are given in Tile units
--- it might make sense to use actual columns
--- and rows for certain operations
-origin = Coords {_x = 1, _y = 1}
-
+module Main (main) where
+import Labyrinth (main)
---
+++ src/Labyrinth.hs
@@ -0,0 +1,23 @@
+module Labyrinth (main) where
+
+import System.Console.ANSI
+import Tile
+import Draw
+import Board
+
+main :: IO ()
+main = do
+  clearScreen
+  drawBackground
+
+  board <- initialBoard
+  drawBoard origin board
+
+  -- this should go elsewhere
+  setCursorPosition gameHeight 0
+
+-- these coordinates are given in Tile units
+-- it might make sense to use actual columns
+-- and rows for certain operations
+origin = Coords {_x = 1, _y = 1}
+
