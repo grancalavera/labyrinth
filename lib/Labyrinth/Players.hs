2d98c8625bba6ced22383eeefcefbfa80a328769
Leon Coto <leoncoto@gmail.com>
2017-10-14 19:07:46 +0100

    Add player handling: player maps and turns

--- lib/Labyrinth/Players.hs
+++ lib/Labyrinth/Players.hs
@@ -1,20 +1,42 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Labyrinth.Players where
+
+import Lens.Micro ((^.))
+import Lens.Micro.TH (makeLenses)
 
 import qualified Data.Map as M
 
 type Name = String
-type PlayerSet = M.Map Color (Maybe Player)
+type Players = M.Map Color Player
 data Color = Yellow | Blue | Green | Red deriving (Show, Eq, Ord)
 
 data Player = Player
-    { _name  :: Name
-    , _color :: Color
-    } deriving (Show)
+    { _color :: Color
+    , _name  :: Name
+    } deriving (Show, Eq)
+makeLenses ''Player
 
-initial :: PlayerSet
-initial = M.fromList
-  [ (Yellow, Nothing)
-  , (Blue, Nothing)
-  , (Green, Nothing)
-  , (Red, Nothing)
-  ]
+initial :: Players
+initial = M.empty
+
+add :: Players -> Player -> Players
+add ps p = M.insert (p ^. color) p ps
+
+addFirst :: Player -> Players
+addFirst = add initial
+
+next :: Players -> Player -> Maybe Player
+next ps currentP = if hasNext
+    then findNext (currentP ^. color)
+    else Nothing
+  where
+    hasNext = M.size ps > 1
+    findNext c = case M.lookup (nextColor c) ps of
+      Just p  -> Just p
+      Nothing -> findNext (nextColor c)
+
+nextColor :: Color -> Color
+nextColor Yellow  = Blue
+nextColor Blue    = Green
+nextColor Green   = Red
+nextColor Red     = Yellow
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,51 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+import qualified Data.Map           as M
+import Test.Hspec
+import Lens.Micro ((^.))
+
+import Labyrinth.Players            (Player(..), Color(..), Players, color)
+import qualified Labyrinth.Players  as Players
+
+main = hspec $ do
+  describe "Players" $ do
+    it "should be initially empty" $ Players.initial `shouldBe`  M.empty
+    it "should add a new player" $ add player1 `shouldBe` Just player1
+    it "should replace an existing player" $ replace playerA `shouldBe` Just playerA
+
+  describe "Turns" $ do
+    it "should not be allowed with 0 players" $ Players.next Players.initial player1 `shouldBe` Nothing
+    it "should not be allowed with 1 player" $ Players.next singlePlayer player1 `shouldBe` Nothing
+    it "should skip 1 non existing player" $ Players.next p1p3 player1 `shouldBe` Just player3
+    it "should skip 2 non existing players" $ Players.next p1p4 player1 `shouldBe` Just player4
+    it "should go back to first player" $ Players.next p1p3 player3 `shouldBe` Just player1
+    it "should move from Yellow to Blue" $ Players.next allPlayers player1 `shouldBe` Just player2
+    it "should move from Blue to Green" $ Players.next allPlayers player2 `shouldBe` Just player3
+    it "should move from Green to Red" $ Players.next allPlayers player3 `shouldBe` Just player4
+    it "should move from Red to Yellow" $ Players.next allPlayers player4 `shouldBe` Just player1
+
+singlePlayer :: Players
+singlePlayer = Players.addFirst player1
+
+p1p3 :: Players
+p1p3 = Players.add singlePlayer player3
+
+p1p4 :: Players
+p1p4 = Players.add singlePlayer player4
+
+allPlayers :: Players
+allPlayers = Players.add (Players.add (Players.add singlePlayer player2) player3) player4
+
+find :: Player -> Players -> Maybe Player
+find p ps = M.lookup (p ^. color) ps
+
+add :: Player -> Maybe Player
+add p = find p (Players.addFirst p)
+
+replace :: Player -> Maybe Player
+replace p = find p (Players.add singlePlayer p)
+
+player1, player2, player3, player4, playerA :: Player
+player1 = Player Yellow  "Yellow 1"
+player2 = Player Blue    "Blue 2"
+player3 = Player Green   "Green 3"
+player4 = Player Red     "Red 4"
+playerA = Player Yellow  "Yellow A"
