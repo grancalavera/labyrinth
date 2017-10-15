bad8b3149c45a58dc11e86bdb7122db4f71bc30f
Leon Coto <leoncoto@gmail.com>
2017-10-15 18:40:56 +0100

    Add Game: initial game and adding players

---
+++ lib/Labyrinth/Game.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Labyrinth.Game where
+
+import Lens.Micro.TH (makeLenses)
+
+import Labyrinth.Players (Player(..), Players)
+import qualified Labyrinth.Players as Players
+
+data Game = Game
+    { _currentPlayer :: Maybe Player
+    , _players :: Players
+    } deriving (Show)
+makeLenses ''Game
+
+initial :: Game
+initial = Game
+    { _currentPlayer = Nothing
+    , _players       = Players.initial
+    }
+
+addPlayer :: Game -> Player -> Game
+addPlayer = undefined
