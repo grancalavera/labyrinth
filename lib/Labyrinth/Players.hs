a8954f26fffaf0238d662ece0180959647b64d19
Leon Coto <leoncoto@gmail.com>
2017-10-18 11:01:20 +0100

    WIP: Refactor `Players` to be Monoid

--- lib/Labyrinth/Players.hs
+++ lib/Labyrinth/Players.hs
@@ -1,25 +1,30 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Labyrinth.Players
     ( Name
-    , Players
+    , Players (..)
     , Player (..)
     , Color (..)
-    , add
-    , addFirst
     , color
-    , initial
     , name
     , next
+    , fromPlayer
+    , lookup
     ) where
 
+import Prelude hiding (lookup)
 import Lens.Micro ((^.))
 import Lens.Micro.TH (makeLenses)
 
 import qualified Data.Map as M
 
 type Name = String
-type Players = M.Map Color Player
 data Color = Yellow | Blue | Green | Red deriving (Show, Eq, Ord)
+
+data Players = Players (M.Map Color Player) deriving (Show, Eq)
+
+instance Monoid Players where
+  Players l `mappend` Players r = Players (M.union r l)
+  mempty = Players M.empty
 
 data Player = Player
     { _color :: Color
@@ -27,24 +32,22 @@ data Player = Player
     } deriving (Show, Eq)
 makeLenses ''Player
 
-initial :: Players
-initial = M.empty
+fromPlayer :: Player -> Players
+fromPlayer p = Players (M.insert  (p ^. color) p mempty)
 
-add :: Player -> Players -> Players
-add p ps = M.insert (p ^. color) p ps
-
-addFirst :: Player -> Players
-addFirst p = add p M.empty
+lookup :: Player -> Players -> Maybe Player
+lookup p (Players ps) = M.lookup (p ^. color) ps
 
 next :: Player -> Players -> Maybe Player
-next currP ps = case hasNext of
-  False -> Nothing
-  True  -> findNext (currP ^. color)
-  where
-    hasNext = M.size ps > 1
-    findNext c = case M.lookup (nextColor c) ps of
-      Just p  -> Just p
-      Nothing -> findNext (nextColor c)
+next =  undefined
+-- next currP ps = case hasNext of
+--   False -> Nothing
+--   True  -> findNext (currP ^. color)
+--   where
+--     hasNext = M.size ps > 1
+--     findNext c = case M.lookup (nextColor c) ps of
+--       Just p  -> Just p
+--       Nothing -> findNext (nextColor c)
 
 nextColor :: Color -> Color
 nextColor Yellow  = Blue
--- test/Test/Labyrinth.hs
+++ test/Test/Labyrinth.hs
@@ -2,45 +2,45 @@ module Test.Labyrinth where
 
 import qualified Data.Map           as M
 import           Lens.Micro         ((^.), (&), (.~))
-import           Labyrinth.Players  (Player(..), Color(..), Players, color)
+import           Labyrinth.Players  (Player(..), Color(..), Players(..), color)
 import qualified Labyrinth.Players  as Players
-import           Labyrinth.Game     (Game(..), currentPlayer)
-import qualified Labyrinth.Game     as Game
+-- import           Labyrinth.Game     (Game(..), currentPlayer)
+-- import qualified Labyrinth.Game     as Game
 
-singlePlayerGame :: Game
-singlePlayerGame = Game.addPlayer player1 Game.initial
+-- singlePlayerGame :: Game
+-- singlePlayerGame = Game.addPlayer player1 Game.initial
 
-allPlayersGame :: Game
-allPlayersGame = g & currentPlayer .~ (Just player1)
-  where
-    g = Game.addPlayer player1
-      $ Game.addPlayer player2
-      $ Game.addPlayer player3
-      $ Game.addPlayer player4 Game.initial
+-- allPlayersGame :: Game
+-- allPlayersGame = g & currentPlayer .~ (Just player1)
+--   where
+--     g = Game.addPlayer player1
+--       $ Game.addPlayer player2
+--       $ Game.addPlayer player3
+--       $ Game.addPlayer player4 Game.initial
 
-singletonPlayers :: Players
-singletonPlayers = Players.addFirst player1
+-- singletonPlayers :: Players
+-- singletonPlayers = Players.addFirst player1
 
-players1And3 :: Players
-players1And3 = Players.add player1 $ Players.addFirst player3
+-- players1And3 :: Players
+-- players1And3 = Players.add player1 $ Players.addFirst player3
 
-players1And4 :: Players
-players1And4 = Players.add player1 $ Players.addFirst player4
+-- players1And4 :: Players
+-- players1And4 = Players.add player1 $ Players.addFirst player4
 
-allPlayers :: Players
-allPlayers = Players.add player1
-  $ Players.add player2
-  $ Players.add player3
-  $ Players.addFirst player4
+-- allPlayers :: Players
+-- allPlayers = Players.add player1
+--   $ Players.add player2
+--   $ Players.add player3
+--   $ Players.addFirst player4
 
 lookupPlayer :: Player -> Players -> Maybe Player
-lookupPlayer p ps = M.lookup (p ^. color) ps
+lookupPlayer p (Players ps) = M.lookup (p ^. color) ps
 
-lookupPlayerInGame :: Player -> Game -> Maybe Player
-lookupPlayerInGame p g = lookupPlayer p (g ^. Game.players)
+-- lookupPlayerInGame :: Player -> Game -> Maybe Player
+-- lookupPlayerInGame p g = lookupPlayer p (g ^. Game.players)
 
-replace :: Player -> Maybe Player
-replace p = lookupPlayer p $ Players.add p $ Players.addFirst player1
+-- replace :: Player -> Maybe Player
+-- replace p = lookupPlayer p $ Players.add p $ Players.addFirst player1
 
 player1, player2, player3, player4, playerA :: Player
 player1 = Player Yellow "Yellow 1"
