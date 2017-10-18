751e35555218d3641bfa9a9a9a4908be8927c1d9
Leon Coto <leoncoto@gmail.com>
2017-10-18 20:29:49 +0100

    WIP: Refactor Players to be a Monoid

--- test/Test/Labyrinth/PlayersSpec.hs
+++ test/Test/Labyrinth/PlayersSpec.hs
@@ -9,42 +9,60 @@ spec :: Spec
 spec = do
   describe "Players" $ do
     it "should be created from a player" $ do
-      let player  = Player Yellow "Foo"
+      let player  = Player Yellow "yellow"
           players = Players.fromPlayer player
       Players.lookupByColor Yellow players `shouldBe` Just player
 
     it "should replace the player on an existing `Color`" $ do
-      let createdWith = Player Yellow "Foo"
-          updatedWith = Player Yellow "Bar"
-          players = Players.fromPlayer createdWith <> Players.fromPlayer updatedWith
+      let createdWith = Player Yellow "yellow 1"
+          updatedWith = Player Yellow "yellow 2"
+          players = Players.fromPlayer createdWith <>
+                    Players.fromPlayer updatedWith
       Players.lookupByColor Yellow players `shouldBe` Just updatedWith
 
   describe "Turns" $ do
-    it "should not be allowed with 0 players" $ do
-      let player = Player Yellow "Foo"
-      Players.next player mempty `shouldBe` Nothing
+    context "when there are less than 2 players" $ do
+      it "should not be allowed with 0 players" $ do
+        let player = Player Yellow "yellow"
+        Players.next player mempty `shouldBe` Nothing
 
-    it "should not be allowed with 1 player" $ do
-      let player  = Player Yellow "Foo"
-          players = Players.fromPlayer player
-      Players.next player players `shouldBe` Nothing
+      it "should not be allowed with 1 player" $ do
+        let player  = Player Yellow "yellow"
+            players = Players.fromPlayer player
+        Players.next player players `shouldBe` Nothing
 
-    it "should skip 1 non existing player" $ do
-      let currentPlayer = Player Yellow "Foo"
-          nextPlayer    = Player Green "Bar"
-          players       = Players.fromPlayer currentPlayer <> Players.fromPlayer nextPlayer
-      Players.next currentPlayer players `shouldBe` Just nextPlayer
+      it "should skip 1 non existing player" $ do
+        let currentPlayer = Player Yellow "yellow"
+            nextPlayer    = Player Green "green"
+            players       = Players.fromPlayer currentPlayer <>
+                            Players.fromPlayer nextPlayer
+        Players.next currentPlayer players `shouldBe` Just nextPlayer
 
-  --   it "should skip 2 non existing players" $
-  --       Players.next player1 players1And4 `shouldBe` Just player4
-  --   it "should go back to first player" $
-  --       Players.next player3 players1And3 `shouldBe` Just player1
-  --   it "should move from Yellow to Blue" $
-  --       Players.next player1 allPlayers `shouldBe` Just player2
-  --   it "should move from Blue to Green" $
-  --       Players.next player2 allPlayers `shouldBe` Just player3
-  --   it "should move from Green to Red" $
-  --       Players.next player3 allPlayers `shouldBe` Just player4
-  --   it "should move from Red to Yellow" $
-  --       Players.next player4 allPlayers `shouldBe` Just player1
+      it "should skip 2 non existing players" $ do
+        let currentPlayer = Player Yellow "yellow"
+            nextPlayer    = Player Red "red"
+            players       = Players.fromPlayer currentPlayer <>
+                            Players.fromPlayer nextPlayer
+        Players.next currentPlayer players `shouldBe` Just nextPlayer
 
+    context "when there are 4 players" $ do
+      let yellow  = Player Yellow "One"
+          blue    = Player Blue "Two"
+          green   = Player Green "Three"
+          red     = Player Red "Four"
+          players = Players.fromPlayer yellow <>
+                    Players.fromPlayer blue   <>
+                    Players.fromPlayer green  <>
+                    Players.fromPlayer red
+
+      it "blue should follow yellow" $
+        Players.next yellow players `shouldBe` Just blue
+
+      it "green should follow blue" $
+        Players.next blue players `shouldBe` Just green
+
+      it "red should follow green" $
+        Players.next green players `shouldBe` Just red
+
+      it "yellow should follow red" $
+        Players.next red players `shouldBe` Just yellow
