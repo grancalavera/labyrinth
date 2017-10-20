ffb3e8ce275cc231d75807b629a8fead09940a08
Leon Coto <leoncoto@gmail.com>
2017-10-20 08:59:01 +0100

    Sketch game tiles

--- lib/Labyrinth/Game.hs
+++ lib/Labyrinth/Game.hs
@@ -3,9 +3,13 @@ module Labyrinth.Game
     ( Game (..)
     , currentPlayer
     , players
+    , currentTile
+    , board
     , playerByColor
     , fromPlayer
     , fromCurrentPlayer
+    , fromBoard
+    , fromCurrentTile
     , nextPlayer
     ) where
 
@@ -16,20 +20,31 @@ import           Lens.Micro.TH        (makeLenses)
 import           Labyrinth.Players    (Player(..), Color(..), Players(..))
 import qualified Labyrinth.Players    as Players
 
+
+-- temp
+type Tile   = ()
+type Board  = ()
+
 data Game = Game
     { _currentPlayer :: Maybe Player
     , _players :: Players
+    , _currentTile :: Maybe Tile
+    , _board :: Board
     } deriving (Show, Eq)
 makeLenses ''Game
 
 instance Monoid Game where
   mempty = Game
-    { _currentPlayer = Nothing
-    , _players = mempty
+    { _currentPlayer  = Nothing
+    , _players        = mempty
+    , _currentTile    = Nothing
+    , _board          = ()
     }
   l `mappend` r = Game
-    { _currentPlayer = (r ^. currentPlayer) <|> (l ^. currentPlayer)
-    , _players       = (l ^. players) <> (r ^. players)
+    { _currentPlayer  = (r ^. currentPlayer) <|> (l ^. currentPlayer)
+    , _players        = (l ^. players) <> (r ^. players)
+    , _currentTile    = Nothing
+    , _board          = ()
     }
 
 playerByColor :: Color -> Game -> Maybe Player
@@ -45,4 +60,11 @@ nextPlayer :: Game -> Maybe Game
 nextPlayer g = do
   currP <- g ^. currentPlayer
   nextP <- Players.next currP (g ^. players)
-  return (g & currentPlayer .~ (Just nextP))
+  return $ g & currentPlayer .~ (Just nextP)
+
+fromBoard :: Board -> Game
+fromBoard = undefined
+
+fromCurrentTile :: Tile -> Game
+fromCurrentTile = undefined
+
