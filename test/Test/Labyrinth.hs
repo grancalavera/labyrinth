a62f299a3026d1264438f3644a77ebb0ef457960
Leon Coto <leoncoto@gmail.com>
2017-10-29 11:29:36 +0000

    Scaffolding for testing Game Monoid

--- test/Test/Labyrinth.hs
+++ test/Test/Labyrinth.hs
@@ -1,11 +1,35 @@
-module Test.Labyrinth (Players) where
+module Test.Labyrinth
+    ( Players
+    , Game
+    , prop_associativity
+    , prop_leftIdentity
+    , prop_rightIdentity
+    ) where
 
 import           Test.QuickCheck
 import           Labyrinth.Players  (Player(..), Color(..), Players, Name)
+import           Labyrinth.Game     (Game(..))
 import qualified Labyrinth.Players  as Players
 import           Control.Monad      (replicateM)
 import           Data.List          (intercalate)
 import           Data.Monoid        ((<>))
+
+instance Arbitrary Players where
+  arbitrary = do
+    players <- genPlayers
+    return $ foldl (<>) mempty players
+
+instance Arbitrary Game where
+  arbitrary = undefined
+
+prop_leftIdentity :: (Monoid a, Eq a) => a -> Bool
+prop_leftIdentity p = p <> mempty == p
+
+prop_rightIdentity :: (Monoid a, Eq a) => a -> Bool
+prop_rightIdentity p = mempty <> p == p
+
+prop_associativity :: (Monoid a, Eq a) => a -> a -> a -> Bool
+prop_associativity x y z = (x <> y) <> z == x <> (y <> z)
 
 genChar :: Gen Char
 genChar = arbitrary
@@ -34,8 +58,3 @@ genPlayers :: Gen [Players]
 genPlayers = do
   i <- choose (1, 16)
   replicateM i (Players.fromPlayer <$> genPlayer)
-
-instance Arbitrary Players where
-  arbitrary = do
-    players <- genPlayers
-    return $ foldl (<>) mempty players
