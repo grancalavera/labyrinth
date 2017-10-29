41c604b77701898745c0b68a6cbfddffae5e68e1
Leon Coto <leoncoto@gmail.com>
2017-10-29 13:56:41 +0000

    Stop working on Game until Tiles a Board are ready

--- lib/Labyrinth/Game.hs
+++ lib/Labyrinth/Game.hs
@@ -3,13 +3,9 @@ module Labyrinth.Game
     ( Game (..)
     , currentPlayer
     , players
-    , currentTile
-    , board
     , playerByColor
     , fromPlayer
     , fromCurrentPlayer
-    , fromBoard
-    , fromCurrentTile
     , nextPlayer
     ) where
 
@@ -20,15 +16,9 @@ import           Lens.Micro.TH        (makeLenses)
 import           Labyrinth.Players    (Player(..), Color(..), Players(..))
 import qualified Labyrinth.Players    as Players
 
--- temp
-data Tile   = Tile deriving (Show, Eq)
-data Board  = BT Tile | Done deriving (Show, Eq)
-
 data Game = Game
     { _currentPlayer :: Maybe Player
     , _players :: Players
-    , _currentTile :: Maybe Tile
-    , _board :: Board
     } deriving (Show, Eq)
 makeLenses ''Game
 
@@ -36,14 +26,10 @@ instance Monoid Game where
   mempty = Game
     { _currentPlayer  = Nothing
     , _players        = mempty
-    , _currentTile    = Nothing
-    , _board          = Done
     }
   l `mappend` r = Game
     { _currentPlayer  = (r ^. currentPlayer) <|> (l ^. currentPlayer)
     , _players        = (l ^. players) <> (r ^. players)
-    , _currentTile    = (r ^. currentTile) <|> (l ^. currentTile)
-    , _board          = Done
     }
 
 playerByColor :: Color -> Game -> Maybe Player
@@ -60,9 +46,3 @@ nextPlayer g = do
   currP <- g ^. currentPlayer
   nextP <- Players.next currP (g ^. players)
   return $ g & currentPlayer .~ (Just nextP)
-
-fromBoard :: Board -> Game
-fromBoard = undefined
-
-fromCurrentTile :: Tile -> Game
-fromCurrentTile = undefined
