ba4c99b333f5d9f69796223fa864a64b211e1ffb
Leon Coto <leoncoto@gmail.com>
2017-11-12 15:31:31 +0000

    Add simple Board

--- lib/Labyrinth/Board.hs
+++ lib/Labyrinth/Board.hs
@@ -1,2 +1,23 @@
-module Labyrinth.Board where
+{-# LANGUAGE TemplateHaskell #-}
+module Labyrinth.Board
+    ( Board
+    , cells
+    , fromCell
+    ) where
 
+import qualified Data.Map         as Map
+import           Data.Map         (Map)
+import           Lens.Micro.TH    (makeLenses)
+import           Labyrinth.Tile   (Tile)
+
+type Position = (Int, Int)
+type Cell = (Position, Tile)
+type Cells = Map Position Tile
+
+data Board = Board
+  { _cells:: Cells
+  } deriving (Show)
+makeLenses ''Board
+
+fromCell :: Cell -> Board
+fromCell (p, t) = Board (Map.insert p t mempty)
