4621d9bb6c397e6bc60a6ed8c106439edf53b54a
Leon Coto <leoncoto@gmail.com>
2017-11-12 18:04:59 +0000

    WIP: Fixed Tile Board

--- lib/Labyrinth/Board.hs
+++ lib/Labyrinth/Board.hs
@@ -1,28 +1,118 @@
-{-# LANGUAGE TemplateHaskell #-}
 module Labyrinth.Board
     ( Board
-    , cells
-    , fromCell
+    , Cell(..)
+    , fixedTiles
     ) where
 
 import qualified Data.Map         as Map
 import           Data.Map         (Map)
-import           Lens.Micro.TH    (makeLenses)
-import           Lens.Micro       ((^.))
-import           Labyrinth.Tile   (Tile)
+import           Data.Monoid      ((<>))
+import           Labyrinth.Tile   (Tile, Terrain(..))
+import qualified Labyrinth.Tile   as Tile
 
 type Position = (Int, Int)
-type Cell = (Position, Tile)
-type Cells = Map Position Tile
+data Cell a = Cell Position a
+data Board a = Board (Map Position a)
 
-data Board = Board
-  { _cells:: Cells
-  } deriving (Show)
-makeLenses ''Board
-
-instance Monoid Board where
+instance Monoid (Board a) where
   mempty = Board mempty
-  l `mappend` r = Board (Map.union (r ^. cells) (l ^. cells))
+  Board l `mappend` Board r = Board (Map.union r l)
 
-fromCell :: Cell -> Board
-fromCell (p, t) = Board (Map.insert p t mempty)
+fromCell :: Cell a -> Board a
+fromCell (Cell p x) = Board (Map.insert p x mempty)
+
+fromCells :: [Cell a] -> Board a
+fromCells cs = foldl (<>) mempty (map fromCell cs)
+
+fixedTiles :: Board Tile
+fixedTiles = fromCells
+  [ Cell (0, 0) (Tile.fromTerrain Blank)
+  , Cell (1, 0) (Tile.fromTerrain Blank)
+  , Cell (2, 0) (Tile.fromTerrain Gate)
+  , Cell (3, 0) (Tile.fromTerrain Blank)
+  , Cell (4, 0) (Tile.fromTerrain Gate)
+  , Cell (5, 0) (Tile.fromTerrain Blank)
+  , Cell (6, 0) (Tile.fromTerrain Gate)
+  , Cell (7, 0) (Tile.fromTerrain Blank)
+  , Cell (8, 0) (Tile.fromTerrain Blank)
+
+  , Cell (0, 1) (Tile.fromTerrain Blank)
+  , Cell (1, 1) (Tile.fromTerrain Blank)
+  , Cell (2, 1) (Tile.fromTerrain Blank)
+  , Cell (3, 1) (Tile.fromTerrain Blank)
+  , Cell (4, 1) (Tile.fromTerrain Blank)
+  , Cell (5, 1) (Tile.fromTerrain Blank)
+  , Cell (6, 1) (Tile.fromTerrain Blank)
+  , Cell (7, 1) (Tile.fromTerrain Blank)
+  , Cell (8, 1) (Tile.fromTerrain Blank)
+
+  , Cell (0, 2) (Tile.fromTerrain Blank)
+  , Cell (1, 2) (Tile.fromTerrain Blank)
+  , Cell (2, 2) (Tile.fromTerrain Blank)
+  , Cell (3, 2) (Tile.fromTerrain Blank)
+  , Cell (4, 2) (Tile.fromTerrain Blank)
+  , Cell (5, 2) (Tile.fromTerrain Blank)
+  , Cell (6, 2) (Tile.fromTerrain Blank)
+  , Cell (7, 2) (Tile.fromTerrain Blank)
+  , Cell (8, 2) (Tile.fromTerrain Blank)
+
+  , Cell (0, 3) (Tile.fromTerrain Blank)
+  , Cell (1, 3) (Tile.fromTerrain Blank)
+  , Cell (2, 3) (Tile.fromTerrain Blank)
+  , Cell (3, 3) (Tile.fromTerrain Blank)
+  , Cell (4, 3) (Tile.fromTerrain Blank)
+  , Cell (5, 3) (Tile.fromTerrain Blank)
+  , Cell (6, 3) (Tile.fromTerrain Blank)
+  , Cell (7, 3) (Tile.fromTerrain Blank)
+  , Cell (8, 3) (Tile.fromTerrain Blank)
+
+  , Cell (0, 4) (Tile.fromTerrain Blank)
+  , Cell (1, 4) (Tile.fromTerrain Blank)
+  , Cell (2, 4) (Tile.fromTerrain Blank)
+  , Cell (3, 4) (Tile.fromTerrain Blank)
+  , Cell (4, 4) (Tile.fromTerrain Blank)
+  , Cell (5, 4) (Tile.fromTerrain Blank)
+  , Cell (6, 4) (Tile.fromTerrain Blank)
+  , Cell (7, 4) (Tile.fromTerrain Blank)
+  , Cell (8, 4) (Tile.fromTerrain Blank)
+
+  , Cell (0, 5) (Tile.fromTerrain Blank)
+  , Cell (1, 5) (Tile.fromTerrain Blank)
+  , Cell (2, 5) (Tile.fromTerrain Blank)
+  , Cell (3, 5) (Tile.fromTerrain Blank)
+  , Cell (4, 5) (Tile.fromTerrain Blank)
+  , Cell (5, 5) (Tile.fromTerrain Blank)
+  , Cell (6, 5) (Tile.fromTerrain Blank)
+  , Cell (7, 5) (Tile.fromTerrain Blank)
+  , Cell (8, 5) (Tile.fromTerrain Blank)
+
+  , Cell (0, 6) (Tile.fromTerrain Blank)
+  , Cell (1, 6) (Tile.fromTerrain Blank)
+  , Cell (2, 6) (Tile.fromTerrain Blank)
+  , Cell (3, 6) (Tile.fromTerrain Blank)
+  , Cell (4, 6) (Tile.fromTerrain Blank)
+  , Cell (5, 6) (Tile.fromTerrain Blank)
+  , Cell (6, 6) (Tile.fromTerrain Blank)
+  , Cell (7, 6) (Tile.fromTerrain Blank)
+  , Cell (8, 6) (Tile.fromTerrain Blank)
+
+  , Cell (0, 7) (Tile.fromTerrain Blank)
+  , Cell (1, 7) (Tile.fromTerrain Blank)
+  , Cell (2, 7) (Tile.fromTerrain Blank)
+  , Cell (3, 7) (Tile.fromTerrain Blank)
+  , Cell (4, 7) (Tile.fromTerrain Blank)
+  , Cell (5, 7) (Tile.fromTerrain Blank)
+  , Cell (6, 7) (Tile.fromTerrain Blank)
+  , Cell (7, 7) (Tile.fromTerrain Blank)
+  , Cell (8, 7) (Tile.fromTerrain Blank)
+
+  , Cell (0, 8) (Tile.fromTerrain Blank)
+  , Cell (1, 8) (Tile.fromTerrain Blank)
+  , Cell (2, 8) (Tile.fromTerrain Gate)
+  , Cell (3, 8) (Tile.fromTerrain Blank)
+  , Cell (4, 8) (Tile.fromTerrain Gate)
+  , Cell (5, 8) (Tile.fromTerrain Blank)
+  , Cell (6, 8) (Tile.fromTerrain Gate)
+  , Cell (7, 8) (Tile.fromTerrain Blank)
+  , Cell (8, 0) (Tile.fromTerrain Blank)
+  ]
