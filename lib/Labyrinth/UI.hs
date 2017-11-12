4bca6f89635f0b497b4a4a3d01683cf7b2ae1895
Leon Coto <leoncoto@gmail.com>
2017-11-12 12:47:47 +0000

    squash

--- lib/Labyrinth/UI.hs
+++ lib/Labyrinth/UI.hs
@@ -2,6 +2,7 @@ module Labyrinth.UI where
 
 import Brick.Widgets.Core (str)
 import Brick.Main         (simpleMain)
+import Brick.Types        (Widget)
 import Data.List          (intercalate)
 
 main :: IO ()
