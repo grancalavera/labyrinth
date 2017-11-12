cd75985be347b5c13788716b6eeee608bb854c91
Leon Coto <leoncoto@gmail.com>
2017-11-12 15:49:06 +0000

    Board Monoid

--- lib/Labyrinth/Board.hs
+++ lib/Labyrinth/Board.hs
@@ -8,6 +8,7 @@ module Labyrinth.Board
 import qualified Data.Map         as Map
 import           Data.Map         (Map)
 import           Lens.Micro.TH    (makeLenses)
+import           Lens.Micro       ((^.))
 import           Labyrinth.Tile   (Tile)
 
 type Position = (Int, Int)
@@ -18,6 +19,10 @@ data Board = Board
   { _cells:: Cells
   } deriving (Show)
 makeLenses ''Board
+
+instance Monoid Board where
+  mempty = Board mempty
+  l `mappend` r = Board (Map.union (r ^. cells) (l ^. cells))
 
 fromCell :: Cell -> Board
 fromCell (p, t) = Board (Map.insert p t mempty)
