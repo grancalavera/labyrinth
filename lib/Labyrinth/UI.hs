66a75c6019041b7c8c2d4565269a705715f4cfad
Leon Coto <leoncoto@gmail.com>
2017-11-18 15:18:10 +0000

    Make patterns for drawing Tile exhaustive 🤘

--- lib/Labyrinth/UI.hs
+++ lib/Labyrinth/UI.hs
@@ -6,14 +6,10 @@ import           Brick.Types        (Widget, Location(..))
 import           Brick.AttrMap      (attrMap)
 import qualified Graphics.Vty       as V
 import           Data.List          (intercalate)
-import           Lens.Micro         ((^.))
-import qualified Data.Set           as Set
 
-import           Labyrinth.Tile     ( Tile
+import           Labyrinth.Tile     ( Tile(..)
                                     , Terrain(..)
-                                    , Edge(..)
-                                    , terrain
-                                    , edges
+                                    , Direction(..)
                                     )
 import qualified Labyrinth.Board    as Board
 import           Labyrinth.Board    (Position)
@@ -37,53 +33,52 @@ ui = map toTile (Board.toList Board.fixedTiles)
     toTile ((x, y), tile) = translateBy (Location (x*7, y*3)) (fromTile tile)
 
 fromTile :: Tile -> Widget ()
-fromTile t = str $ intercalate "\n" $ case (tileTerrain, tileEdges) of
-  (Blank, _) ->                   ["       ",
-                                   "       ",
-                                   "       "]
-  (Gate, [North]) ->              ["       ",
-                                   "   ▲   ",
-                                   "       "]
-  (Gate, [South]) ->              ["       ",
-                                   "   ▼   ",
-                                   "       "]
-  (Gate, [West]) ->               ["       ",
-                                   "  ◄    ",
-                                   "       "]
-  (Gate, [East]) ->               ["       ",
-                                   "    ►  ",
-                                   "       "]
-  (Corner, [North, West]) ->      ["─┘   │ ",
-                                   "     │ ",
-                                   "─────┘ "]
-  (Corner, [North, East]) ->      [" │   └─",
-                                   " │     ",
-                                   " └─────"]
-  (Corner, [South, East]) ->      [" ┌─────",
-                                   " │     ",
-                                   " │   ┌─"]
-  (Corner, [West, South]) ->      ["─────┐ ",
-                                   "     │ ",
-                                   "─┐   │ "]
-  (Fork, [North, South, East]) -> [" │   └─",
-                                   " │     ",
-                                   " │   ┌─"]
-  (Fork, [North, West, South]) -> ["─┘   │ ",
-                                   "     │ ",
-                                   "─┐   │ "]
-  (Fork, [North, West, East]) ->  ["─┘   └─",
-                                   "       ",
-                                   "───────"]
-  (Fork, [West, South, East]) ->  ["───────",
-                                   "       ",
-                                   "─┐   ┌─"]
-  (Path, [North, South]) ->       [" │   │ ",
-                                   " │   │ ",
-                                   " │   │ "]
-  (Path, [West, East]) ->         ["───────",
-                                   "       ",
-                                   "───────"]
-  (_, _) ->                       error "unknown tile"
+fromTile tile = str $ intercalate "\n" $ case tile of
+
+  Tile Gate North   ->  ["       ",
+                         "   ▲   ",
+                         "       "]
+  Tile Gate South   ->  ["       ",
+                         "   ▼   ",
+                         "       "]
+  Tile Gate West    ->  ["       ",
+                         "  ◄    ",
+                         "       "]
+  Tile Gate East    ->  ["       ",
+                         "    ►  ",
+                         "       "]
+  Tile Corner North ->  ["─┘   │ ",
+                         "     │ ",
+                         "─────┘ "]
+  Tile Corner West  ->  ["─────┐ ",
+                         "     │ ",
+                         "─┐   │ "]
+  Tile Corner East  ->  [" │   └─",
+                         " │     ",
+                         " └─────"]
+  Tile Corner South ->  [" ┌─────",
+                         " │     ",
+                         " │   ┌─"]
+  Tile Fork East    ->  [" │   └─",
+                         " │     ",
+                         " │   ┌─"]
+  Tile Fork West    ->  ["─┘   │ ",
+                         "     │ ",
+                         "─┐   │ "]
+  Tile Fork North   ->  ["─┘   └─",
+                         "       ",
+                         "───────"]
+  Tile Fork South   ->  ["───────",
+                         "       ",
+                         "─┐   ┌─"]
+  Tile Path North -> vpath
+  Tile Path South -> vpath
+  Tile Path West  -> hpath
+  Tile Path East  -> hpath
   where
-    tileTerrain = t ^. terrain
-    tileEdges   = Set.toList (t ^. edges)
+    vpath =             [" │   │ ",
+                         " │   │ ",
+                         " │   │ "]
+    hpath =             ["───────",
+                         "       ",
+                         "───────"]
