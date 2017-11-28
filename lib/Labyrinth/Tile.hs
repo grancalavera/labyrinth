f04818cd71e631bd94dcac51199e42d8e60adb84
Leon Coto <leoncoto@gmail.com>
2017-11-28 14:33:25 +0000

    Give an Tile, return its edges

--- lib/Labyrinth/Tile.hs
+++ lib/Labyrinth/Tile.hs
@@ -4,7 +4,11 @@ module Labyrinth.Tile
     , Terrain(..)
     , rotate
     , rotate'
+    , edges
     ) where
+
+import           Data.Set (Set)
+import qualified Data.Set as Set
 
 data Direction =  North
                 | West
@@ -25,6 +29,27 @@ rotate (Tile t d) = Tile t (nextDirection d)
 
 rotate' :: Tile -> Tile
 rotate' = mirror . rotate
+
+edges :: Tile -> Set Direction
+edges t = Set.fromList $ case t of
+  Tile Gate North   -> [North]
+  Tile Gate South   -> [South]
+  Tile Gate West    -> [West]
+  Tile Gate East    -> [East]
+  Tile Corner North -> [North, West]
+  Tile Corner West  -> [West, South]
+  Tile Corner East  -> [East, North]
+  Tile Corner South -> [South, East]
+  Tile Fork East    -> [North, South, East]
+  Tile Fork West    -> [North, West, South]
+  Tile Fork South   -> [West, South, East]
+  Tile Path North   -> vpath
+  Tile Path South   -> vpath
+  Tile Path West    -> hpath
+  Tile Path East    -> hpath
+  where
+    vpath = [North, South]
+    hpath = [West, East]
 
 mirror :: Tile -> Tile
 mirror (Tile t d) = Tile t $ (nextDirection . nextDirection) d
