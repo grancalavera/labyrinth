6f295ba9ac15660f662e6e0524a0c8083e5e9165
Leon Coto <leoncoto@gmail.com>
2017-11-28 14:31:52 +0000

    Cleanup: remove redundant functions

--- lib/Labyrinth/Tile.hs
+++ lib/Labyrinth/Tile.hs
@@ -23,17 +23,14 @@ data Tile = Tile Terrain Direction deriving (Show, Eq)
 rotate :: Tile -> Tile
 rotate (Tile t d) = Tile t (nextDirection d)
 
-mirror :: Tile -> Tile
-mirror (Tile t d) = Tile t (oppositeDirection d)
-
 rotate' :: Tile -> Tile
 rotate' = mirror . rotate
+
+mirror :: Tile -> Tile
+mirror (Tile t d) = Tile t $ (nextDirection . nextDirection) d
 
 nextDirection :: Direction -> Direction
 nextDirection North  = West
 nextDirection West   = South
 nextDirection South  = East
 nextDirection East   = North
-
-oppositeDirection :: Direction -> Direction
-oppositeDirection = nextDirection . nextDirection
