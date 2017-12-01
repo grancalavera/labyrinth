6a0efe451f62f80b457e1e5964bbcaf20b439db1
Leon Coto <leoncoto@gmail.com>
2017-12-01 12:10:16 +0000

    Render initial board using Brick 🤘

--- lib/Labyrinth/Game.hs
+++ lib/Labyrinth/Game.hs
@@ -12,25 +12,27 @@ module Labyrinth.Game
     , initialGame
 
     -- temp
-    , movableTiles
+    , movableCells
     , movablePositions
     -- temp
 
     ) where
 
-import           Data.Monoid          ((<>))
-import           Control.Applicative  ((<|>))
-import           Lens.Micro           ((^.), (&), (%~), (.~))
-import           Lens.Micro.TH        (makeLenses)
--- import qualified Data.Array.IO        as ArrayIO
--- import           Control.Monad        (forM)
--- import           System.Random        (randomRIO)
-import qualified Labyrinth.Players    as Players
-import           Labyrinth.Players    (Player(..), Color(..), Players(..))
-import qualified Labyrinth.Board      as Board
-import           Labyrinth.Board      (Board, Position)
-import           Labyrinth.Tile       (Tile(..), Terrain(..), Direction(..))
-import qualified Labyrinth.Cell       as Cell
+import           Data.Monoid         ((<>))
+import           Control.Applicative ((<|>))
+import           Lens.Micro          ((^.), (&), (%~), (.~))
+import           Lens.Micro.TH       (makeLenses)
+import qualified Data.Array.IO       as AIO
+import           Data.Array.IO       (IOArray)
+import           Control.Monad       (forM)
+import           System.Random       (randomRIO)
+import qualified Labyrinth.Players   as Players
+import           Labyrinth.Players   (Player(..), Color(..), Players(..))
+import qualified Labyrinth.Board     as Board
+import           Labyrinth.Board     (Board, Position)
+import           Labyrinth.Tile      (Tile(..), Terrain(..), Direction(..))
+import qualified Labyrinth.Cell      as Cell
+import           Labyrinth.Cell      (Cell)
 
 data Game = Game
     { _currentPlayer  :: Maybe Player
@@ -70,42 +72,56 @@ nextPlayer g = do
   return $ g & currentPlayer .~ (Just nextP)
 
 initialGame :: IO Game
-initialGame = return (fromBoard fixedTiles)
+initialGame = do
+  b <- initialBoard
+  return (fromBoard b)
 
-fixedTiles :: Board
-fixedTiles = Board.fromList
-  [ ((2, 0), Cell.fromTile (Tile Gate South))
-  , ((4, 0), Cell.fromTile (Tile Gate South))
-  , ((6, 0), Cell.fromTile (Tile Gate South))
-  , ((0, 2), Cell.fromTile (Tile Gate East))
-  , ((0, 4), Cell.fromTile (Tile Gate East))
-  , ((0, 6), Cell.fromTile (Tile Gate East))
-  , ((8, 2), Cell.fromTile (Tile Gate West))
-  , ((8, 4), Cell.fromTile (Tile Gate West))
-  , ((8, 6), Cell.fromTile (Tile Gate West))
-  , ((2, 8), Cell.fromTile (Tile Gate North))
-  , ((4, 8), Cell.fromTile (Tile Gate North))
-  , ((6, 8), Cell.fromTile (Tile Gate North))
-  , ((1, 1), Cell.fromTile (Tile Corner South))
-  , ((7, 1), Cell.fromTile (Tile Corner West))
-  , ((1, 7), Cell.fromTile (Tile Corner East))
-  , ((7, 7), Cell.fromTile (Tile Corner North))
-  , ((3, 1), Cell.fromTile (Tile Fork South))
-  , ((5, 1), Cell.fromTile (Tile Fork South))
-  , ((1, 3), Cell.fromTile (Tile Fork East))
-  , ((1, 5), Cell.fromTile (Tile Fork East))
-  , ((7, 3), Cell.fromTile (Tile Fork West))
-  , ((7, 5), Cell.fromTile (Tile Fork West))
-  , ((3, 7), Cell.fromTile (Tile Fork North))
-  , ((5, 7), Cell.fromTile (Tile Fork North))
-  , ((3, 3), Cell.fromTile (Tile Fork East))
-  , ((5, 3), Cell.fromTile (Tile Fork South))
-  , ((3, 5), Cell.fromTile (Tile Fork North))
-  , ((5, 5), Cell.fromTile (Tile Fork West))
-  ]
+initialBoard :: IO Board
+initialBoard = do
+  ps      <- shuffle movablePositions
+  (_:cs)  <- shuffle movableCells
+  do
+    let fixed       = fixedPositionsAndTiles
+        movable     = zip ps cs
+        positioned  = map tileToCell (fixed ++ movable)
+    return (Board.fromList positioned)
 
-movableTiles :: [Tile]
-movableTiles =
+tileToCell :: (Position, Tile) -> (Position, Cell)
+tileToCell (p, t) = (p, Cell.fromTile t)
+
+fixedPositionsAndTiles :: [(Position, Tile)]
+fixedPositionsAndTiles =  [ ((2, 0), Tile Gate South)
+                          , ((4, 0), Tile Gate South)
+                          , ((6, 0), Tile Gate South)
+                          , ((0, 2), Tile Gate East)
+                          , ((0, 4), Tile Gate East)
+                          , ((0, 6), Tile Gate East)
+                          , ((8, 2), Tile Gate West)
+                          , ((8, 4), Tile Gate West)
+                          , ((8, 6), Tile Gate West)
+                          , ((2, 8), Tile Gate North)
+                          , ((4, 8), Tile Gate North)
+                          , ((6, 8), Tile Gate North)
+                          , ((1, 1), Tile Corner South)
+                          , ((7, 1), Tile Corner West)
+                          , ((1, 7), Tile Corner East)
+                          , ((7, 7), Tile Corner North)
+                          , ((3, 1), Tile Fork South)
+                          , ((5, 1), Tile Fork South)
+                          , ((1, 3), Tile Fork East)
+                          , ((1, 5), Tile Fork East)
+                          , ((7, 3), Tile Fork West)
+                          , ((7, 5), Tile Fork West)
+                          , ((3, 7), Tile Fork North)
+                          , ((5, 7), Tile Fork North)
+                          , ((3, 3), Tile Fork East)
+                          , ((5, 3), Tile Fork South)
+                          , ((3, 5), Tile Fork North)
+                          , ((5, 5), Tile Fork West)
+                          ]
+
+movableCells :: [Tile]
+movableCells =
   replicate 12 (Tile Path North) ++
   replicate 16 (Tile Corner North) ++
   replicate 6  (Tile Fork North)
@@ -115,16 +131,16 @@ movablePositions =
   [(x,y) | x <- [2,4,6], y <- [1,3,5,7]] ++
   [(x,y) | x <- [1..7], y <- [2, 4, 6]]
 
--- shuffle :: [a] -> IO [a]
--- shuffle xs = do
---   ar <- newArray n xs
---   forM [1..n] $ \i -> do
---     j <- randomRIO (i, n)
---     vi <- ArrayIO.readArray ar i
---     vj <- ArrayIO.readArray ar j
---     ArrayIO.writeArray ar j vi
---     return vj
---   where
---     n = length xs
---     newArray :: Int -> [a] -> IO (IOArray Int a)
---     newArray n' xs' = ArrayIO.newListArray (1, n') xs'
+shuffle :: [a] -> IO [a]
+shuffle xs = do
+  ar <- newArray n xs
+  forM [1..n] $ \i -> do
+    j <- randomRIO (i, n)
+    vi <- AIO.readArray ar i
+    vj <- AIO.readArray ar j
+    AIO.writeArray ar j vi
+    return vj
+  where
+    n = length xs
+    newArray :: Int -> [a] -> IO (IOArray Int a)
+    newArray n' xs' = AIO.newListArray (1, n') xs'
