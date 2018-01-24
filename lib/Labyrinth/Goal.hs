dbcce2b7c49f04719250104b0df098e00036f34e
Leon Coto <leoncoto@gmail.com>
2018-01-24 21:17:05 +0000

    Enumerate treasures

--- lib/Labyrinth/Goal.hs
+++ lib/Labyrinth/Goal.hs
@@ -2,12 +2,16 @@ module Labyrinth.Goal (
     Goal (..)
   , Found
   , Treasure (..)
+  , treasures
   ) where
 
 data Treasure = Bat |  Beetle | Book | Chandelier | Chest | Crown | Dragon
               | Emerald | Genie | Ghost | Goblin | Gold | Helmet | Keys
               | Lizzard | Map | Moth | Owl | Princess | Rat | Ring | Skull
-              | Spider | Sword deriving (Eq, Show)
+              | Spider | Sword deriving (Eq, Show, Enum)
 
 type Found = Bool
 data Goal = Goal Treasure Found deriving (Eq, Show)
+
+treasures :: [Treasure]
+treasures = [Bat ..]
