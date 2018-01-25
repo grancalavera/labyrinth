9e4662e63bd256914beeef9cdf3a4bc6aada2c54
Leon Coto <leoncoto@gmail.com>
2018-01-25 18:48:24 +0000

    Make Treasure instance of Ord so that it can be used as key in Map

--- lib/Labyrinth/Goal.hs
+++ lib/Labyrinth/Goal.hs
@@ -9,7 +9,7 @@ module Labyrinth.Goal (
 data Treasure = Bat |  Beetle | Book | Chandelier | Chest | Crown | Dragon
               | Emerald | Genie | Ghost | Goblin | Gold | Helmet | Keys
               | Lizzard | Map | Moth | Owl | Princess | Rat | Ring | Skull
-              | Spider | Sword deriving (Eq, Show, Enum)
+              | Spider | Sword deriving (Eq, Show, Enum, Ord)
 
 type Found = Bool
 data Goal = Goal Treasure Found deriving (Eq, Show)
