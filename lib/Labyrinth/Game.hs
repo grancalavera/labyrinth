54f4c99b4febf5af013eb610de30631cba1f3ea7
Leon Coto <leoncoto@gmail.com>
2018-02-24 14:30:39 +0000

    Replace initial game generation method :)

--- lib/Labyrinth/Game.hs
+++ lib/Labyrinth/Game.hs
@@ -9,7 +9,6 @@ module Labyrinth.Game
     , colSpread
     , nextPlayer
     , initialGame
-    , playerColorByDefaultPosition
     -- temp
     , mkBoard
     , mkEnv
@@ -29,7 +28,6 @@ import           Control.Applicative ((<|>))
 import           Control.Monad       (mapM)
 import qualified Data.Map.Strict     as Map
 import           Data.Map.Strict     (Map)
-import           Data.Maybe          (fromMaybe)
 import           Lens.Micro          ((^.), (&), (.~))
 import           Lens.Micro.TH       (makeLenses)
 import qualified Labyrinth           as Labyrinth
@@ -38,12 +36,8 @@ import qualified Labyrinth.Players   as Players
 import           Labyrinth.Players   (Player(..), Color(..), Players(..))
 import qualified Labyrinth.Direction as Direction
 import           Labyrinth.Direction (Direction(..))
-import qualified Labyrinth.Tile      as Tile
 import           Labyrinth.Tile      ( Tile(..)
                                      , Terrain(..)
-                                     , goal
-                                     , terrain
-                                     , tenants
                                      )
 import           Labyrinth.Gate      (Gate(..))
 import qualified Labyrinth.Goal      as Goal
@@ -86,75 +80,11 @@ chooseNonEmpty [] x = x
 chooseNonEmpty x [] = x
 chooseNonEmpty x _ = x
 
---------------------------------------------------------------------------------
--- games
---------------------------------------------------------------------------------
-
-initialGame :: Players -> IO Game
-initialGame pls = do
-  g <- basicGame pls
-  return $ g & tiles .~ (
-       mempty
-    <> insertPlayer (7,7) pls (g ^. tiles)
-    <> insertPlayer (1,1) pls (g ^. tiles)
-    <> insertPlayer (7,1) pls (g ^. tiles)
-    <> insertPlayer (1,7) pls (g ^. tiles)
-    )
-
-
-basicGame :: Players -> IO Game
-basicGame pls = do
-  ps <- Labyrinth.shuffle movingPositions
-  mt <- Labyrinth.shuffle movingTiles >>= mapM Tile.randomRotate
-  sp <- Labyrinth.shuffle $ Players.toList pls
-  ts <- Labyrinth.shuffle Goal.treasures
-
-  let fixedTiles = Map.fromList tileList
-      movingTiles' = Map.fromList $ zip (defaultCurrentTilePosition:ps) mt
-      (goals1, goals2, goals3) = distribute $ map Goal.fromTreasure ts
-
-  return $ Game
-    { _players = pls
-    , _currentPlayer = Just (snd (sp !! 0))
-    , _rowSpread = [0..8]
-    , _colSpread = [0..8]
-    , _gates = Map.fromList gateList
-    , _currentTilePosition = Just defaultCurrentTilePosition
-    , _tiles = mempty
-        <> addGoals (Labyrinth.filterByPositions fixedGoalPositions fixedTiles) goals1
-        <> addGoals (filterByTerrain Corner movingTiles') goals2
-        <> addGoals (filterByTerrain Fork movingTiles') goals3
-        <> fixedTiles
-        <> movingTiles'
-    }
-
 distribute :: [a] -> ([a], [a], [a])
 distribute gs = (x, y, z)
   where
     (x, x') = Labyrinth.halve gs
     (y, z)  = Labyrinth.halve x'
-
-insertPlayer :: Position -> Players -> Map Position Tile -> Map Position Tile
-insertPlayer pos pls board = fromMaybe board $ do
-  c <- Map.lookup pos playerColorByDefaultPosition
-  p <- Players.lookup c pls
-  t <- Map.lookup pos board
-  return $ Map.insert pos (t & tenants .~ [p]) board
-
-playerColorByDefaultPosition :: Map Position Color
-playerColorByDefaultPosition = Map.fromList $ zip [(1,1), (7,1), (7,7), (1,7)] Players.colors
-
-addGoals :: Map Position Tile -> [Goal] -> Map Position Tile
-addGoals m gs = Map.fromList $ map addGoal $ zip (Map.toList m) gs
-  where
-    addGoal :: ((Position, Tile), Goal) -> (Position, Tile)
-    addGoal ((p, t), g) = (p, t & goal .~ Just g)
-
-filterByTerrain :: Terrain -> Map Position Tile -> Map Position Tile
-filterByTerrain trn = Map.filter byTerrain
-  where
-    byTerrain :: Tile -> Bool
-    byTerrain t = trn == t ^. terrain
 
 --------------------------------------------------------------------------------
 -- players
@@ -170,9 +100,6 @@ nextPlayer g = do
 -- boards
 --------------------------------------------------------------------------------
 
-defaultCurrentTilePosition :: Position
-defaultCurrentTilePosition = (2,0)
-
 gateList :: [(Position, Gate)]
 gateList = [ ((2, 0), Gate South True)
             , ((4, 0), Gate South True)
@@ -187,55 +114,6 @@ gateList = [ ((2, 0), Gate South True)
             , ((4, 8), Gate North True)
             , ((6, 8), Gate North True)
             ]
-
-tileList :: [(Position, Tile)]
-tileList = [ ((1, 1), Tile Corner South Nothing [])
-           , ((7, 1), Tile Corner West Nothing [])
-           , ((1, 7), Tile Corner East Nothing [])
-           , ((7, 7), Tile Corner North Nothing [])
-           , ((3, 1), Tile Fork South Nothing [])
-           , ((5, 1), Tile Fork South Nothing [])
-           , ((1, 3), Tile Fork East Nothing [])
-           , ((1, 5), Tile Fork East Nothing [])
-           , ((7, 3), Tile Fork West Nothing [])
-           , ((7, 5), Tile Fork West Nothing [])
-           , ((3, 7), Tile Fork North Nothing [])
-           , ((5, 7), Tile Fork North Nothing [])
-           , ((3, 3), Tile Fork East Nothing [])
-           , ((5, 3), Tile Fork South Nothing [])
-           , ((3, 5), Tile Fork North Nothing [])
-           , ((5, 5), Tile Fork West Nothing [])
-           ]
-
-movingTiles :: [Tile]
-movingTiles = replicate 12 (Tile Path North Nothing []) ++
-              replicate 16 (Tile Corner North Nothing []) ++
-              replicate 6  (Tile Fork North Nothing [])
-
-movingPositions :: [Position]
-movingPositions =
-  [(x,y) | x <- [2,4,6], y <- [1,3,5,7]] ++
-  [(x,y) | x <- [1..7], y <- [2, 4, 6]]
-
-fixedGoalPositions :: [Position]
-fixedGoalPositions = [ (3, 1)
-                     , (5, 1)
-                     , (1, 3)
-                     , (3, 3)
-                     , (5, 3)
-                     , (7, 3)
-                     , (1, 5)
-                     , (3, 5)
-                     , (5, 5)
-                     , (7, 5)
-                     , (3, 7)
-                     , (5, 7)
-                     ]
-
-
-
-
-
 
 --------------------------------------------------------------------------------
 -- StateT based game
@@ -377,3 +255,20 @@ mkEnv plys = do
     cs = 16
     fs = 6
     ps = 12
+
+--------------------------------------------------------------------------------
+-- games
+--------------------------------------------------------------------------------
+
+initialGame :: Players -> IO Game
+initialGame players' = do
+  tiles' <- mkBoard players'
+  return $ Game
+    { _players = players'
+    , _gates = Map.fromList gateList
+    , _tiles = Map.fromList tiles'
+    , _currentPlayer = Nothing
+    , _rowSpread = [0..8]
+    , _colSpread = [0..8]
+    , _currentTilePosition = Just (2,0)
+    }
