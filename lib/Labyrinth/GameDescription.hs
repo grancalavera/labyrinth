8baf5fa08e054bf4bcdafed1a18eb441d18023b6
Leon Coto <leoncoto@gmail.com>
2018-02-25 22:03:56 +0000

    Write down instructions for new solution

--- lib/Labyrinth/GameDescription.hs
+++ lib/Labyrinth/GameDescription.hs
@@ -47,6 +47,88 @@ import           Labyrinth.Goal       (Treasure(..), Goal)
 
 data GoalDescription = AddGoal | MaybeGoal | NoGoal deriving (Show)
 
+{-
+data TileDescription = TD
+  { _dPosition  :: Maybe Position
+  , _dTerrain   :: Terrain
+  , _dDirection :: Maybe Direction
+  , _dTreasure  :: Maybe Treasure
+  , _dPlayer    :: Maybe Player
+  }
+
+--------------------------------------------------------------------------------
+-- 16 fully known tiles
+--------------------------------------------------------------------------------
+ 1 TD (Just (1,1)) Corner  (Just South)  False (Just Yellow)
+ 2 TD (Just (3,1)) Fork    (Just East)   True  Nothing
+ 3 TD (Just (5,1)) Fork    (Just East)   True  Nothing
+ 4 TD (Just (7,1)) Corner  (Just West)   False (Just Red)
+ 5 TD (Just (1,3)) Fork    (Just East)   True  Nothing
+ 6 TD (Just (3,3)) Fork    (Just East)   True  Nothing
+ 7 TD (Just (5,3)) Fork    (Just South)  True  Nothing
+ 8 TD (Just (7,3)) Fork    (Just West)   True  Nothing
+ 9 TD (Just (1,5)) Fork    (Just East)   True  Nothing
+10 TD (Just (3,5)) Fork    (Just North)  True  Nothing
+11 TD (Just (5,5)) Fork    (Just West)   True  Nothing
+12 TD (Just (7,5)) Fork    (Just West)   True  Nothing
+13 TD (Just (1,7)) Corner  (Just East)   False (Just Green)
+14 TD (Just (3,7)) Fork    (Just North)  True  Nothing
+15 TD (Just (5,7)) Fork    (Just North)  True  Nothing
+16 TD (Just (7,7)) Corner  (Just North)  False (Just Blue)
+--------------------------------------------------------------------------------
+-- 12 randomly placed paths
+--------------------------------------------------------------------------------
+17 TD Nothing      Path    Nothing       False Nothing
+18 TD Nothing      Path    Nothing       False Nothing
+19 TD Nothing      Path    Nothing       False Nothing
+20 TD Nothing      Path    Nothing       False Nothing
+21 TD Nothing      Path    Nothing       False Nothing
+22 TD Nothing      Path    Nothing       False Nothing
+23 TD Nothing      Path    Nothing       False Nothing
+24 TD Nothing      Path    Nothing       False Nothing
+25 TD Nothing      Path    Nothing       False Nothing
+26 TD Nothing      Path    Nothing       False Nothing
+27 TD Nothing      Path    Nothing       False Nothing
+28 TD Nothing      Path    Nothing       False Nothing
+--------------------------------------------------------------------------------
+-- 6 randomly placed corners with treasures
+--------------------------------------------------------------------------------
+29 TD Nothing      Corner  Nothing       True  Nothing
+30 TD Nothing      Corner  Nothing       True  Nothing
+31 TD Nothing      Corner  Nothing       True  Nothing
+32 TD Nothing      Corner  Nothing       True  Nothing
+33 TD Nothing      Corner  Nothing       True  Nothing
+34 TD Nothing      Corner  Nothing       True  Nothing
+--------------------------------------------------------------------------------
+-- 10 randomly placed corners
+--------------------------------------------------------------------------------
+35 TD Nothing      Corner  Nothing       False Nothing
+36 TD Nothing      Corner  Nothing       False Nothing
+37 TD Nothing      Corner  Nothing       False Nothing
+38 TD Nothing      Corner  Nothing       False Nothing
+39 TD Nothing      Corner  Nothing       False Nothing
+40 TD Nothing      Corner  Nothing       False Nothing
+41 TD Nothing      Corner  Nothing       False Nothing
+42 TD Nothing      Corner  Nothing       False Nothing
+43 TD Nothing      Corner  Nothing       False Nothing
+44 TD Nothing      Corner  Nothing       False Nothing
+--------------------------------------------------------------------------------
+-- 6 randomly placed forks with treasures
+--------------------------------------------------------------------------------
+45 TD Nothing      Fork    Nothing       True  Nothing
+46 TD Nothing      Fork    Nothing       True  Nothing
+47 TD Nothing      Fork    Nothing       True  Nothing
+48 TD Nothing      Fork    Nothing       True  Nothing
+49 TD Nothing      Fork    Nothing       True  Nothing
+50 TD Nothing      Fork    Nothing       True  Nothing
+
+1. start by listing all the positions [(x,y)|x<-[1..7], y<-[1..7]]++[(2,0)]
+2. filter out the positions of the known tiles
+3. shuffle the resulting positions
+4. fill the blanks tile description by tile description, tearing out
+   1 shuffled position at a time from the Env
+-}
+
 data TileDescription = TD
   { _dPosition  :: Position
   , _dTerrain   :: Maybe Terrain
