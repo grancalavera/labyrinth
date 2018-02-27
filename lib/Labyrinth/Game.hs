e68d9056f66f71aa37ad7f490278e7296626a5a8
Leon Coto <leoncoto@gmail.com>
2018-02-27 21:39:43 +0000

    Generate tiles using GameDescription

--- lib/Labyrinth/Game.hs
+++ lib/Labyrinth/Game.hs
@@ -12,29 +12,25 @@ module Labyrinth.Game
     , initialGame
     ) where
 
-import           Data.Monoid         ((<>))
-import           Control.Monad       (mapM)
-import qualified Data.Map.Strict     as Map
-import           Data.Map.Strict     (Map)
-import           Lens.Micro          ((^.), (&), (.~))
-import           Lens.Micro.TH       (makeLenses)
-import qualified Labyrinth           as Labyrinth
-import           Labyrinth           (Position)
-import qualified Labyrinth.Players   as Players
-import           Labyrinth.Players   ( Player(..)
-                                     , Color(..)
-                                     , Players(..)
-                                     )
-import qualified Labyrinth.Direction as Direction
-import           Labyrinth.Direction (Direction(..))
-import           Labyrinth.Tile      ( Tile(..)
-                                     , Terrain(..)
-                                     )
-import           Labyrinth.Gate      (Gate(..))
-import qualified Labyrinth.Goal      as Goal
-import           Labyrinth.Goal      (Goal)
-
-import Control.Monad.State           (StateT, evalStateT, get, put)
+import qualified Data.Map.Strict           as Map
+import           Data.Map.Strict           (Map)
+import           Lens.Micro                ((^.), (&), (.~))
+import           Lens.Micro.TH             (makeLenses)
+import           Labyrinth                 (Position)
+import qualified Labyrinth.Players         as Players
+import           Labyrinth.Players         ( Player(..)
+                                           , Color(..)
+                                           , Players(..)
+                                           )
+import           Labyrinth.Direction       (Direction(..))
+import           Labyrinth.Tile            ( Tile(..)
+                                           , Terrain(..)
+                                           )
+import           Labyrinth.Gate            (Gate(..))
+import           Labyrinth.GameDescription ( GameDescription(..)
+                                           , TileDescription(..)
+                                           , mkTiles
+                                           )
 
 data Game = Game
     { _currentPlayer       :: Maybe Player
@@ -47,188 +43,109 @@ data Game = Game
     } deriving (Show, Eq)
 makeLenses ''Game
 
-data Env = Env
-  { _eShuffledTiles :: [(Terrain, Direction)]
-  , _eSetGoals      :: [Maybe Goal]
-  , _eCornerGoals   :: [Maybe Goal]
-  , _eForkGoals     :: [Maybe Goal]
-  , _ePlayers       :: Players
-  } deriving (Show)
-makeLenses ''Env
-
-type Eval a = StateT Env IO a
-type TileDescription = (Position, TD)
-
--- tile description: know Tile or Random Tile
-data TD = T (Terrain, Direction, GD, PD) | RT (GD, PD) deriving (Show)
--- goal description: Goal, No Goal, Maybe Goal
-data GD = G | MG | NG deriving (Show)
--- player description: Yellow Player, Green Player, Blue Player, Red Player, No Player
-data PD = P Color | NP deriving (Show)
-
---------------------------------------------------------------------------------
--- game control
---------------------------------------------------------------------------------
-
 initialGame :: Players -> IO Game
 initialGame players' = do
-  tiles'         <- mkTiles players'
+  tiles' <- mkTiles GD { _dTiles     = tiles''
+                       , _dPlayers   = players'
+                       , _dPositions = (2,0):[(x,y) | x <- [1..7], y <- [1..7]]
+                       }
   currentPlayer' <- Players.first players'
+
   return $ Game
     { _currentPlayer = currentPlayer'
     , _currentTilePosition = (2,0)
     , _players = players'
-    , _gates = Map.fromList gateList
+    , _gates = Map.fromList gates'
     , _tiles = Map.fromList tiles'
     , _rowSpread = [0..8]
     , _colSpread = [0..8]
     }
+
+  where
+    gates'  =
+      [ ((2, 0), Gate South True)
+      , ((4, 0), Gate South True)
+      , ((6, 0), Gate South True)
+      , ((0, 2), Gate East True)
+      , ((0, 4), Gate East True)
+      , ((0, 6), Gate East True)
+      , ((8, 2), Gate West True)
+      , ((8, 4), Gate West True)
+      , ((8, 6), Gate West True)
+      , ((2, 8), Gate North True)
+      , ((4, 8), Gate North True)
+      , ((6, 8), Gate North True)
+      ]
+    tiles'' =
+      ----------------------------------------
+      -- 16 fully known tiles
+      ----------------------------------------
+      [ TD Corner  (Just (1,1)) (Just South)  False (Just Yellow)
+      , TD Fork    (Just (3,1)) (Just East)   True  Nothing
+      , TD Fork    (Just (5,1)) (Just East)   True  Nothing
+      , TD Corner  (Just (7,1)) (Just West)   False (Just Red)
+      , TD Fork    (Just (1,3)) (Just East)   True  Nothing
+      , TD Fork    (Just (3,3)) (Just East)   True  Nothing
+      , TD Fork    (Just (5,3)) (Just South)  True  Nothing
+      , TD Fork    (Just (7,3)) (Just West)   True  Nothing
+      , TD Fork    (Just (1,5)) (Just East)   True  Nothing
+      , TD Fork    (Just (3,5)) (Just North)  True  Nothing
+      , TD Fork    (Just (5,5)) (Just West)   True  Nothing
+      , TD Fork    (Just (7,5)) (Just West)   True  Nothing
+      , TD Corner  (Just (1,7)) (Just East)   False (Just Green)
+      , TD Fork    (Just (3,7)) (Just North)  True  Nothing
+      , TD Fork    (Just (5,7)) (Just North)  True  Nothing
+      , TD Corner  (Just (7,7)) (Just North)  False (Just Blue)
+      ----------------------------------------
+      -- 12 randomly placed paths
+      ----------------------------------------
+      , TD Path    Nothing      Nothing       False Nothing
+      , TD Path    Nothing      Nothing       False Nothing
+      , TD Path    Nothing      Nothing       False Nothing
+      , TD Path    Nothing      Nothing       False Nothing
+      , TD Path    Nothing      Nothing       False Nothing
+      , TD Path    Nothing      Nothing       False Nothing
+      , TD Path    Nothing      Nothing       False Nothing
+      , TD Path    Nothing      Nothing       False Nothing
+      , TD Path    Nothing      Nothing       False Nothing
+      , TD Path    Nothing      Nothing       False Nothing
+      , TD Path    Nothing      Nothing       False Nothing
+      , TD Path    Nothing      Nothing       False Nothing
+      ----------------------------------------
+      -- 6 randomly placed corners with treasures
+      ----------------------------------------
+      , TD Corner  Nothing      Nothing       True  Nothing
+      , TD Corner  Nothing      Nothing       True  Nothing
+      , TD Corner  Nothing      Nothing       True  Nothing
+      , TD Corner  Nothing      Nothing       True  Nothing
+      , TD Corner  Nothing      Nothing       True  Nothing
+      , TD Corner  Nothing      Nothing       True  Nothing
+      ----------------------------------------
+      -- 10 randomly placed corners
+      ----------------------------------------
+      , TD Corner  Nothing      Nothing       False Nothing
+      , TD Corner  Nothing      Nothing       False Nothing
+      , TD Corner  Nothing      Nothing       False Nothing
+      , TD Corner  Nothing      Nothing       False Nothing
+      , TD Corner  Nothing      Nothing       False Nothing
+      , TD Corner  Nothing      Nothing       False Nothing
+      , TD Corner  Nothing      Nothing       False Nothing
+      , TD Corner  Nothing      Nothing       False Nothing
+      , TD Corner  Nothing      Nothing       False Nothing
+      , TD Corner  Nothing      Nothing       False Nothing
+      ----------------------------------------
+      -- 6 randomly placed forks with treasures
+      ----------------------------------------
+      , TD Fork    Nothing      Nothing       True  Nothing
+      , TD Fork    Nothing      Nothing       True  Nothing
+      , TD Fork    Nothing      Nothing       True  Nothing
+      , TD Fork    Nothing      Nothing       True  Nothing
+      , TD Fork    Nothing      Nothing       True  Nothing
+      , TD Fork    Nothing      Nothing       True  Nothing
+      ]
 
 nextPlayer :: Game -> Maybe Game
 nextPlayer g = do
   currP <- g ^. currentPlayer
   nextP <- Players.next currP (g ^. players)
   return $ g & currentPlayer .~ (Just nextP)
-
---------------------------------------------------------------------------------
--- boards
---------------------------------------------------------------------------------
-
-gateList :: [(Position, Gate)]
-gateList =
-  [ ((2, 0), Gate South True)
-  , ((4, 0), Gate South True)
-  , ((6, 0), Gate South True)
-  , ((0, 2), Gate East True)
-  , ((0, 4), Gate East True)
-  , ((0, 6), Gate East True)
-  , ((8, 2), Gate West True)
-  , ((8, 4), Gate West True)
-  , ((8, 6), Gate West True)
-  , ((2, 8), Gate North True)
-  , ((4, 8), Gate North True)
-  , ((6, 8), Gate North True)
-  ]
-
-tileDescriptions :: [TileDescription]
-tileDescriptions = Map.toList $ (Map.fromList
-  -- first fill the rows we know
-  -- row 1
-  [ ((1,1), T (Corner, South, NG, P Yellow))
-  , ((3,1), T (Fork, East, G, NP))
-  , ((5,1), T (Fork, East, G, NP))
-  , ((7,1), T (Corner, West, NG, P Red))
-  -- row 3
-  , ((1,3), T (Fork, East, G, NP))
-  , ((3,3), T (Fork, East, G, NP))
-  , ((5,3), T (Fork, South, G, NP))
-  , ((7,3), T (Fork, West, G, NP))
-  -- row 5
-  , ((1,5), T (Fork, East, G, NP))
-  , ((3,5), T (Fork, North, G, NP))
-  , ((5,5), T (Fork, West, G, NP))
-  , ((7,5), T (Fork, West, G, NP))
-  -- row 7
-  , ((1,7), T (Corner, East, NG, P Green))
-  , ((3,7), T (Fork, North, G, NP))
-  , ((5,7), T (Fork, North, G, NP))
-  , ((7,7), T (Corner, North, NG, P Blue))
-  ])
-  -- then fill the rest randomly
-  <> (Map.fromList $ rt (2,0) : [rt (x,y) | x <- [1..7], y <- [1..7]])
-  where rt p = (p, RT (MG, NP))
-
---------------------------------------------------------------------------------
--- tile's board construction
---------------------------------------------------------------------------------
-
-mkTiles :: Players -> IO [(Position, Tile)]
-mkTiles players' = mkEnv players' >>= \ env -> evalStateT (eval tileDescriptions) env
-
-mkEnv :: Players -> IO Env
-mkEnv players' = do
-  ts <- Labyrinth.shuffle $ replicate ps Path ++
-                            replicate cs Corner ++
-                            replicate fs Fork
-  ds <- mapM (const Direction.random) [1..(cs+fs+ps)]
-  gs <- Labyrinth.shuffle $ map (Just . Goal.fromTreasure) Goal.treasures
-
-  let (sg, cg, fg) = distribute gs
-
-  -- only 6 corners have goals so we fill the remaining 6 with Nothing
-  -- so that we can just pick thaem one by one without worrying they
-  -- might be empty, and shuffle again
-  cg' <- Labyrinth.shuffle $ cg ++ replicate 6 Nothing
-
-  return $ Env
-    { _eShuffledTiles = zip ts ds
-    , _eSetGoals      = sg
-    , _eCornerGoals   = cg'
-    , _eForkGoals     = fg
-    , _ePlayers       = players'
-    }
-
-  where
-    cs, fs, ps :: Int
-    cs = 16
-    fs = 6
-    ps = 12
-    distribute :: [a] -> ([a], [a], [a])
-    distribute gs = (x, y, z)
-      where
-        (x, x') = Labyrinth.halve gs
-        (y, z)  = Labyrinth.halve x'
-
-eval :: [TileDescription] -> Eval [(Position, Tile)]
-eval = mapM (\(pos, instruction) -> case instruction of
-    T desc  -> do
-      t <- evalTile desc
-      return $ (pos, t)
-    RT desc -> do
-      t <- evalRandomTile desc
-      return $ (pos, t)
-  )
-
-evalTile :: (Terrain, Direction, GD, PD) -> Eval Tile
-evalTile (terrain', direction', goalDesc, playerDesc) = do
-  goal' <- evalGoal terrain' goalDesc
-  player' <- evalPlayer playerDesc
-  return $ Tile terrain' direction' goal' player'
-
-evalRandomTile :: (GD, PD) -> Eval Tile
-evalRandomTile (goalDesc, playerDesc) = do
-  env <- get
-  let ((terrain', direction'):ts) = env ^. eShuffledTiles
-  put (env & eShuffledTiles .~ ts)
-  goal' <- evalGoal terrain' goalDesc
-  player' <- evalPlayer playerDesc
-  return $ Tile terrain' direction' goal' player'
-
-evalGoal :: Terrain -> GD -> Eval (Maybe Goal)
-evalGoal _ NG = return Nothing
-evalGoal _ G  = do
-  env <- get
-  case (env ^. eSetGoals) of
-    []     -> return Nothing
-    (g:gs) -> put (env & eSetGoals .~ gs) >> return g
-evalGoal Corner MG = do
-  env <- get
-  case (env ^. eCornerGoals) of
-    []     -> return Nothing
-    (g:gs) -> put (env & eCornerGoals .~ gs) >> return g
-evalGoal Fork   MG = do
-  env <- get
-  case (env ^. eForkGoals) of
-    []     -> return Nothing
-    (g:gs) -> put (env & eForkGoals .~ gs) >> return g
-evalGoal _      MG = return Nothing
-
-evalPlayer :: PD -> Eval [Player]
-evalPlayer NP = return []
-evalPlayer (P color') = do
-  env <- get
-  case (Players.lookup color' (env ^. ePlayers)) of
-    Just plyr  -> return [plyr]
-    Nothing -> return []
-
-
