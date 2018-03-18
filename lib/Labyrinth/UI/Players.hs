f66895c2fb383a74670ddf43841aa1172c8fdea8
Leon Coto <leoncoto@gmail.com>
2018-03-18 16:01:11 +0000

    Initial half-working players form

--- lib/Labyrinth/UI/Players.hs
+++ lib/Labyrinth/UI/Players.hs
@@ -27,6 +27,7 @@ import           Brick.Focus          ( focusGetCurrent
                                       )
 import qualified Brick.Widgets.Edit   as E
 import qualified Brick.Widgets.Center as C
+import qualified Brick.Widgets.Border as B
 
 import qualified Labyrinth.Players    as Players
 import           Labyrinth.Players    (Players, Player(..), Color(..))
@@ -35,7 +36,7 @@ data PlayersInfo = PlayersInfo
   { _p1 :: T.Text
   , _p2 :: T.Text
   , _p3 :: T.Text
-  , _P4 :: T.Text
+  , _p4 :: T.Text
   } deriving (Show)
 makeLenses ''PlayersInfo
 
@@ -45,17 +46,10 @@ data Name = P1Field
           | P4Field
           deriving (Eq, Ord, Show)
 
-addPlayers :: IO Players
-addPlayers = return $ fromJust $ Players.fromList
-  [ Player Yellow "Yellow Player"
-  , Player Blue "Blue Player"
-  , Player Green "Green Player"
-  , Player Red "Red Player"
-  ]
-
 mkForm :: PlayersInfo -> Form PlayersInfo e Name
 mkForm =
-  let label s w = padBottom (Pad 1) $ (vLimit 1 $ hLimit 15 $ str s <+> fill ' ') <+> w
+  let label s w = padBottom (Pad 1)
+                    $ (vLimit 1 $ hLimit 15 $ str s <+> fill ' ') <+> w
   in newForm
     [ label "Player 1" @@= editTextField p1 P1Field (Just 16)
     , label "Player 2" @@= editTextField p1 P2Field (Just 16)
@@ -63,24 +57,47 @@ mkForm =
     , label "Player 4" @@= editTextField p1 P4Field (Just 16)
     ]
 
+theMap :: AttrMap
+theMap = attrMap V.defAttr
+  [ (E.editAttr,           V.white `on` V.black)
+  , (E.editFocusedAttr,    V.black `on` V.brightBlack)
+  , (focusedFormInputAttr, V.black `on` V.brightBlack)
+  ]
 
-{-
-mkForm :: UserInfo -> Form UserInfo e Name
-mkForm =
-    let label s w = padBottom (Pad 1)
-      $   (vLimit 1 $ hLimit 15 $ str s <+> fill ' ')
-      <+> w
-    in newForm
-      [ label "Name"          @@= editTextField name NameField (Just 1)
-      , label "Address"       @@= B.borderWithLabel (str "Mailing")
-                                @@= editTextField address AddressField (Just 3)
-      , label "Age"           @@= editShowableField age AgeField
-      , label "Password"      @@= editPasswordField password PasswordField
-      , label "Dominant hand" @@= radioField handed
-                                [ (LeftHanded, LeftHandField, "Left")
-                                , (RightHanded, RightHandField, "Right")
-                                , (Ambidextrous, AmbiField, "Both")
-                                ]
-      , label ""              @@= checkboxField ridesBike BikeField "etc"
-               ]
--}
+draw :: Form PlayersInfo e Name -> [Widget Name]
+draw f = [C.vCenter $ C.hCenter form]
+  where
+    form = B.border $ padTop (Pad 1) $ hLimit 15 $ renderForm f
+
+app :: App (Form PlayersInfo e Name) e Name
+app = App
+  { appDraw         = draw
+  , appHandleEvent  = handleEvent
+  , appChooseCursor = focusRingCursor formFocus
+  , appStartEvent   = return
+  , appAttrMap      = const theMap
+  }
+
+handleEvent :: Form PlayersInfo e Name
+  -> BrickEvent Name e
+  -> EventM Name (Next (Form PlayersInfo e Name))
+handleEvent s e = case e of
+  VtyEvent (V.EvKey V.KEsc [])        -> halt s
+  VtyEvent (V.EvKey (V.KChar 'q') []) -> halt s
+  _                            -> continue s
+
+addPlayers :: IO Players
+addPlayers = do
+  let initialState = PlayersInfo { _p1 = ""
+                                 , _p2 = ""
+                                 , _p3 = ""
+                                 , _p4 = ""
+                                 }
+  Brick.defaultMain app $ mkForm initialState
+
+  return $ fromJust $ Players.fromList
+    [ Player Yellow "Yellow Player"
+    , Player Blue "Blue Player"
+    , Player Green "Green Player"
+    , Player Red "Red Player"
+    ]
