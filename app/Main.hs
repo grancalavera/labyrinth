fee1a372dbc76de418123eb4f1e3d68ffa401038
Leon Coto <leoncoto@gmail.com>
2018-04-01 23:13:18 +0100

    Cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -37,21 +37,14 @@ fastGame players = do
                         , ((6, 2), Gate North True)
                         , ((6, 4), Gate North True)
                         ]
-    , _gTiles         = [
-                        -- corners
-                          DTile Corner (Just (1, 1)) (Just South) False (Just Yellow)
+    , _gTiles         = [ DTile Corner (Just (1, 1)) (Just South) False (Just Yellow)
                         , DTile Corner (Just (1, 5)) (Just West)  False (Just Red)
                         , DTile Corner (Just (5, 1)) (Just East)  False (Just Green)
                         , DTile Corner (Just (5, 5)) (Just North) False (Just Blue)
-                        -- top border
                         , DTile Fork   (Just (1, 3)) (Just South) True  Nothing
-                        -- right border
                         , DTile Fork   (Just (3, 1)) (Just East)  True  Nothing
-                        -- left border
                         , DTile Fork   (Just (3, 5)) (Just West)  True  Nothing
-                        -- bottom border
                         , DTile Fork   (Just (5, 3)) (Just North) True  Nothing
-                        -- center
                         , DTile Fork   (Just (3, 3)) Nothing      True  Nothing
                         ]
       ++ replicate 5 (DTile Path Nothing Nothing False Nothing)
@@ -71,49 +64,31 @@ regularGame players = do
     , _gRowMax        = 8
     , _gColMin        = 0
     , _gColMax        = 8
-    , _gGates         = [
-                        -- top gates
-                          ((0, 2), Gate South True)
+    , _gGates         = [ ((0, 2), Gate South True)
                         , ((0, 4), Gate South True)
-                        , ( (0, 6)
-                          , Gate South True
-                          )
-                        -- left gates
+                        , ((0, 6), Gate South True)
                         , ((2, 0), Gate East True)
                         , ((4, 0), Gate East True)
-                        , ( (6, 0)
-                          , Gate East True
-                          )
-                        -- right gates
+                        , ((6, 0), Gate East True)
                         , ((2, 8), Gate West True)
                         , ((4, 8), Gate West True)
-                        , ( (6, 8)
-                          , Gate West True
-                          )
-                        -- bottom gates
+                        , ((6, 8), Gate West True)
                         , ((8, 2), Gate North True)
                         , ((8, 4), Gate North True)
                         , ((8, 6), Gate North True)
                         ]
-    , _gTiles         = [
-                        -- corners
-                          DTile Corner (Just (1, 1)) (Just South) False (Just Yellow)
+    , _gTiles         = [ DTile Corner (Just (1, 1)) (Just South) False (Just Yellow)
                         , DTile Corner (Just (1, 7)) (Just West)  False (Just Red)
                         , DTile Corner (Just (7, 1)) (Just East)  False (Just Green)
                         , DTile Corner (Just (7, 7)) (Just North) False (Just Blue)
-                        -- top border
                         , DTile Fork   (Just (1, 3)) (Just South) True  Nothing
                         , DTile Fork   (Just (1, 5)) (Just South) True  Nothing
-                        -- right border
                         , DTile Fork   (Just (3, 1)) (Just East)  True  Nothing
                         , DTile Fork   (Just (5, 1)) (Just East)  True  Nothing
-                        -- left border
                         , DTile Fork   (Just (3, 7)) (Just West)  True  Nothing
                         , DTile Fork   (Just (5, 7)) (Just West)  True  Nothing
-                        -- bottom border
                         , DTile Fork   (Just (7, 3)) (Just North) True  Nothing
                         , DTile Fork   (Just (7, 5)) (Just North) True  Nothing
-                        -- center
                         , DTile Fork   (Just (3, 3)) (Just East)  True  Nothing
                         , DTile Fork   (Just (5, 3)) (Just North) True  Nothing
                         , DTile Fork   (Just (3, 5)) (Just South) True  Nothing
