f60c7ae8b41b7078e143d241ee7ccdbb4bc7a729
Leon Coto <leoncoto@gmail.com>
2018-04-01 23:06:45 +0100

    Smaller testing game

--- app/Main.hs
+++ app/Main.hs
@@ -19,71 +19,108 @@ main = addPlayers >>= fastGame >>= playGame
 
 fastGame :: Players -> IO Game
 fastGame players = do
-    let startPosition = (0, 2)
-    Labyrinth.makeGame DGame
-        { _gPlayers       = players
-        , _gStartPosition = startPosition
-        , _gTreasures     = take 4 Labyrinth.treasures
-        , _gRowMin        = 0
-        , _gColMin        = 0
-        , _gRowMax        = 4
-        , _gColMax        = 4
-        , _gGates         = [ ((0, 2), Gate South True)
-                            , ((2, 0), Gate East True)
-                            , ((2, 4), Gate West True)
-                            , ((4, 2), Gate North True)
-                            ]
-        , _gTiles = [ DTile Corner (Just (1, 1)) (Just South) False (Just Yellow)
-                    , DTile Corner (Just (1, 3)) (Just West)  False (Just Red)
-                    , DTile Corner (Just (3, 1)) (Just East)  False (Just Green)
-                    , DTile Corner (Just (3, 3)) (Just North) False (Just Blue)
-                    ]
-            ++ replicate 6 (DTile Path Nothing Nothing True Nothing)
-        }
+  let startPosition = (0, 2)
+  Labyrinth.makeGame DGame
+    { _gPlayers       = players
+    , _gStartPosition = startPosition
+    , _gTreasures     = take 12 Labyrinth.treasures
+    , _gRowMin        = 0
+    , _gColMin        = 0
+    , _gRowMax        = 6
+    , _gColMax        = 6
+    , _gGates         = [ ((0, 2), Gate South True)
+                        , ((0, 4), Gate South True)
+                        , ((2, 0), Gate East True)
+                        , ((4, 0), Gate East True)
+                        , ((2, 6), Gate West True)
+                        , ((4, 6), Gate West True)
+                        , ((6, 2), Gate North True)
+                        , ((6, 4), Gate North True)
+                        ]
+    , _gTiles         = [
+                        -- corners
+                          DTile Corner (Just (1, 1)) (Just South) False (Just Yellow)
+                        , DTile Corner (Just (1, 5)) (Just West)  False (Just Red)
+                        , DTile Corner (Just (5, 1)) (Just East)  False (Just Green)
+                        , DTile Corner (Just (5, 5)) (Just North) False (Just Blue)
+                        -- top border
+                        , DTile Fork   (Just (1, 3)) (Just South) True  Nothing
+                        -- right border
+                        , DTile Fork   (Just (3, 1)) (Just East)  True  Nothing
+                        -- left border
+                        , DTile Fork   (Just (3, 5)) (Just West)  True  Nothing
+                        -- bottom border
+                        , DTile Fork   (Just (5, 3)) (Just North) True  Nothing
+                        -- center
+                        , DTile Fork   (Just (3, 3)) Nothing      True  Nothing
+                        ]
+      ++ replicate 5 (DTile Path Nothing Nothing False Nothing)
+      ++ replicate 5 (DTile Corner Nothing Nothing False Nothing)
+      ++ replicate 4 (DTile Corner Nothing Nothing True Nothing)
+      ++ replicate 3 (DTile Fork Nothing Nothing True Nothing)
+    }
 
 regularGame :: Players -> IO Game
 regularGame players = do
-    let startPosition = (0, 2)
-    Labyrinth.makeGame DGame
-        { _gPlayers       = players
-        , _gStartPosition = startPosition
-        , _gTreasures     = Labyrinth.treasures
-        , _gRowMin        = 0
-        , _gRowMax        = 8
-        , _gColMin        = 0
-        , _gColMax        = 8
-        , _gGates         = [ ((0, 2), Gate South True)
-                            , ((0, 4), Gate South True)
-                            , ((0, 6), Gate South True)
-                            , ((2, 0), Gate East True)
-                            , ((4, 0), Gate East True)
-                            , ((6, 0), Gate East True)
-                            , ((2, 8), Gate West True)
-                            , ((4, 8), Gate West True)
-                            , ((6, 8), Gate West True)
-                            , ((8, 2), Gate North True)
-                            , ((8, 4), Gate North True)
-                            , ((8, 6), Gate North True)
-                            ]
-        , _gTiles = [ DTile Corner (Just (1, 1)) (Just South) False (Just Yellow)
-                    , DTile Fork   (Just (1, 3)) (Just South) True  Nothing
-                    , DTile Fork   (Just (1, 5)) (Just South) True  Nothing
-                    , DTile Corner (Just (1, 7)) (Just West)  False (Just Red)
-                    , DTile Fork   (Just (3, 1)) (Just East)  True  Nothing
-                    , DTile Fork   (Just (3, 3)) (Just East)  True  Nothing
-                    , DTile Fork   (Just (3, 5)) (Just South) True  Nothing
-                    , DTile Fork   (Just (3, 7)) (Just West)  True  Nothing
-                    , DTile Fork   (Just (5, 1)) (Just East)  True  Nothing
-                    , DTile Fork   (Just (5, 3)) (Just North) True  Nothing
-                    , DTile Fork   (Just (5, 5)) (Just West)  True  Nothing
-                    , DTile Fork   (Just (5, 7)) (Just West)  True  Nothing
-                    , DTile Corner (Just (7, 1)) (Just East)  False (Just Green)
-                    , DTile Fork   (Just (7, 3)) (Just North) True  Nothing
-                    , DTile Fork   (Just (7, 5)) (Just North) True  Nothing
-                    , DTile Corner (Just (7, 7)) (Just North) False (Just Blue)
-                    ]
-            ++ replicate 12 (DTile Path Nothing Nothing False Nothing)
-            ++ replicate 6  (DTile Corner Nothing Nothing True Nothing)
-            ++ replicate 10 (DTile Corner Nothing Nothing False Nothing)
-            ++ replicate 6  (DTile Fork Nothing Nothing True Nothing)
-        }
+  let startPosition = (0, 2)
+  Labyrinth.makeGame DGame
+    { _gPlayers       = players
+    , _gStartPosition = startPosition
+    , _gTreasures     = Labyrinth.treasures
+    , _gRowMin        = 0
+    , _gRowMax        = 8
+    , _gColMin        = 0
+    , _gColMax        = 8
+    , _gGates         = [
+                        -- top gates
+                          ((0, 2), Gate South True)
+                        , ((0, 4), Gate South True)
+                        , ( (0, 6)
+                          , Gate South True
+                          )
+                        -- left gates
+                        , ((2, 0), Gate East True)
+                        , ((4, 0), Gate East True)
+                        , ( (6, 0)
+                          , Gate East True
+                          )
+                        -- right gates
+                        , ((2, 8), Gate West True)
+                        , ((4, 8), Gate West True)
+                        , ( (6, 8)
+                          , Gate West True
+                          )
+                        -- bottom gates
+                        , ((8, 2), Gate North True)
+                        , ((8, 4), Gate North True)
+                        , ((8, 6), Gate North True)
+                        ]
+    , _gTiles         = [
+                        -- corners
+                          DTile Corner (Just (1, 1)) (Just South) False (Just Yellow)
+                        , DTile Corner (Just (1, 7)) (Just West)  False (Just Red)
+                        , DTile Corner (Just (7, 1)) (Just East)  False (Just Green)
+                        , DTile Corner (Just (7, 7)) (Just North) False (Just Blue)
+                        -- top border
+                        , DTile Fork   (Just (1, 3)) (Just South) True  Nothing
+                        , DTile Fork   (Just (1, 5)) (Just South) True  Nothing
+                        -- right border
+                        , DTile Fork   (Just (3, 1)) (Just East)  True  Nothing
+                        , DTile Fork   (Just (5, 1)) (Just East)  True  Nothing
+                        -- left border
+                        , DTile Fork   (Just (3, 7)) (Just West)  True  Nothing
+                        , DTile Fork   (Just (5, 7)) (Just West)  True  Nothing
+                        -- bottom border
+                        , DTile Fork   (Just (7, 3)) (Just North) True  Nothing
+                        , DTile Fork   (Just (7, 5)) (Just North) True  Nothing
+                        -- center
+                        , DTile Fork   (Just (3, 3)) (Just East)  True  Nothing
+                        , DTile Fork   (Just (5, 3)) (Just North) True  Nothing
+                        , DTile Fork   (Just (3, 5)) (Just South) True  Nothing
+                        , DTile Fork   (Just (5, 5)) (Just West)  True  Nothing
+                        ]
+      ++ replicate 12 (DTile Path Nothing Nothing False Nothing)
+      ++ replicate 6  (DTile Corner Nothing Nothing True Nothing)
+      ++ replicate 10 (DTile Corner Nothing Nothing False Nothing)
+      ++ replicate 6  (DTile Fork Nothing Nothing True Nothing)
+    }
