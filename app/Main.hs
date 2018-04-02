2473eb6cd6d8b4908e3dd767df6391857753d65b
Leon Coto <leoncoto@gmail.com>
2018-04-02 14:09:10 +0100

    Remove fastGame

--- app/Main.hs
+++ app/Main.hs
@@ -47,42 +47,6 @@ testGame players = do
                         ]
     }
 
-fastGame :: Players -> IO Game
-fastGame players = do
-  let startPosition = (0, 2)
-  Labyrinth.gameFromDescription DGame
-    { _gPlayers       = players
-    , _gStartPosition = startPosition
-    , _gTreasures     = take 12 Labyrinth.treasures
-    , _gRowMin        = 0
-    , _gColMin        = 0
-    , _gRowMax        = 6
-    , _gColMax        = 6
-    , _gGates         = [ ((0, 2), Gate South True)
-                        , ((0, 4), Gate South True)
-                        , ((2, 0), Gate East True)
-                        , ((4, 0), Gate East True)
-                        , ((2, 6), Gate West True)
-                        , ((4, 6), Gate West True)
-                        , ((6, 2), Gate North True)
-                        , ((6, 4), Gate North True)
-                        ]
-    , _gTiles         = [ DTile Corner (Just (1, 1)) (Just South) False (Just Yellow)
-                        , DTile Corner (Just (1, 5)) (Just West)  False (Just Red)
-                        , DTile Corner (Just (5, 1)) (Just East)  False (Just Green)
-                        , DTile Corner (Just (5, 5)) (Just North) False (Just Blue)
-                        , DTile Fork   (Just (1, 3)) (Just South) True  Nothing
-                        , DTile Fork   (Just (3, 1)) (Just East)  True  Nothing
-                        , DTile Fork   (Just (3, 5)) (Just West)  True  Nothing
-                        , DTile Fork   (Just (5, 3)) (Just North) True  Nothing
-                        , DTile Fork   (Just (3, 3)) Nothing      True  Nothing
-                        ]
-      ++ replicate 5 (DTile Path Nothing Nothing False Nothing)
-      ++ replicate 5 (DTile Corner Nothing Nothing False Nothing)
-      ++ replicate 4 (DTile Corner Nothing Nothing True Nothing)
-      ++ replicate 3 (DTile Fork Nothing Nothing True Nothing)
-    }
-
 regularGame :: Players -> IO Game
 regularGame players = do
   let startPosition = (0, 2)
