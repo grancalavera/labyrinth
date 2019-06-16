272843a2614177f25b3d60237909fb4f132b62ba
Leon Coto <leoncoto@gmail.com>
2019-06-16 12:27:41 +0200

    remove redundant event

--- lib/Labyrinth/Store.hs
+++ lib/Labyrinth/Store.hs
@@ -5,7 +5,6 @@ module Labyrinth.Store
   , state
   , modals
   , initial
-  , isModalEvent
   , isShowingModal
   , nextModal
   )
@@ -14,16 +13,11 @@ where
 import           Brick
 import           Control.Lens                                       ( (^.) )
 import           Labyrinth.Store.Internal
-import           Labyrinth.UI                                       ( Name )
-
 import           Labyrinth.UI.Modal                                 ( Modal )
 import qualified Labyrinth.UI.Modal            as Modal
 
 initial :: Store e
 initial = Store { _state = Splash, _modals = [welcome] }
-
-isModalEvent :: Store e -> BrickEvent Name e -> Bool
-isModalEvent store _ = isShowingModal store
 
 isShowingModal :: Store e -> Bool
 isShowingModal = not . null . (^. modals)
