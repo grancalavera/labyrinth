1fb6e8c98fe5dd4048aa5982e0989707f1ec33e7
Leon Coto <leoncoto@gmail.com>
2019-06-17 09:06:10 +0200

    rename game module

--- lib/Labyrinth/Game.hs
+++ lib/Labyrinth/Game.hs
@@ -10,13 +10,13 @@ module Labyrinth.Game
   )
 where
 
-import           Labyrinth.Game.Class                     ( Game(..)
-                                                          , playing
-                                                          )
-import           Labyrinth.Game.Configuration             ( Configuration )
-import           Labyrinth.Game.Player                    ( Color(..)
-                                                          , PlayOrder(..)
-                                                          , Player(..)
-                                                          , Players
-                                                          )
-import           Labyrinth.Game.Position                  ( Position )
+import           Labyrinth.Game.Internal                            ( Game(..)
+                                                                    , playing
+                                                                    )
+import           Labyrinth.Game.Configuration                       ( Configuration )
+import           Labyrinth.Game.Player                              ( Color(..)
+                                                                    , PlayOrder(..)
+                                                                    , Player(..)
+                                                                    , Players
+                                                                    )
+import           Labyrinth.Game.Position                            ( Position )
